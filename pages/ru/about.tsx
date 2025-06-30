16553fcf8ff11b3e4c80a1004db0e6c8f6067b10
sakitibi <sakitibi10nin@icloud.com>
2025-06-30 12:26:42 +0900

    コミットされました

--- pages/ru/about.tsx
+++ pages/ru/about.tsx
@@ -6,33 +6,33 @@ export default function Home() {
         <>
             <Head>
                 <meta charSet='UTF-8'/>
-                <title>当レンタルWikiについて</title>
+                <title>Об этом вики-сайте по аренде</title>
             </Head>
             <main style={{ padding: '2rem' }}>
-                <h1>当レンタルWikiについて</h1>
-                <p>当Wiki(以下あさクラWiki)は</p>
-                <p>オープンソースで<a href='https://wikiwiki.jp' target='_blank'>WIKIWIKI</a>などより</p>
-                <p>使いやすいレンタルWikiサービスを目指しています。</p>
-                <p><a href='https://github.com/sakitibi/AsakuraWiki' target='_blank'>ソースはここ</a></p>
+                <h1>Об этом вики-сайте по аренде</h1>
+                <p>Эта Wiki (далее именуемая Asakura Wiki)</p>
+                <p>Открытый исходный код, например<a href='https://wikiwiki.jp' target='_blank'>WIKIWIKI</a></p>
+                <p>Наша цель — предоставить простую в использовании услугу аренды Wiki.</p>
+                <p><a href='https://github.com/sakitibi/AsakuraWiki' target='_blank'>Источник здесь</a></p>
             </main>
             <footer>
                 <div id="menu">
                     <ul>
                         <li>
-                            <Link href="/">
-                                <button><span>ホーム</span></button>
+                            <Link href="/ru">
+                                <button><span>Дом</span></button>
                             </Link>
                         </li>
                         <li>
-                            <Link href="/about">
-                                <button><span>当レンタルWikiについて</span></button>
+                            <Link href="/ru/about">
+                                <button><span>Об этом вики-сайте по аренде</span></button>
                             </Link>
                         </li>
                     </ul>
                 </div>
                 <div style={{textAlign: "center"}}>
                     <p>Copyright 2025 13ninstudio All rights Reserved</p>
-                    <p>当Wikiサービスはオープンソースプロジェクトです</p>
+                    <p>Этот Wiki-сервис — проект с открытым исходным кодом.</p>
                 </div>
             </footer>
         </>
