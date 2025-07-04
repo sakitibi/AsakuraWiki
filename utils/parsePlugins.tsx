aa33422a65f8c6eabafdc412edf13461fe0787dc
sakitibi <sakitibi10nin@icloud.com>
2025-07-04 20:14:30 +0900

    コミットされました

--- utils/parsePlugins.tsx
+++ utils/parsePlugins.tsx
@@ -126,6 +126,9 @@ function Accordion({ title, level, initiallyOpen, children, }: { title: string; 
         : 'M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32zM200 344l0-64-64 0c-13.3 0-24-10.7-24-24s10.7-24 24-24l64 0 0-64c0-13.3 10.7-24 24-24s24 10.7 24 24l0 64 64 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-64 0 0 64c0 13.3-10.7c24-24 c24s-24-10.7-24-24z'
     const headingStyle: React.CSSProperties = level === '*'
     ? {
+        color: '#000',
+        margin: '.2em 0 .5em',
+        padding: '.3em .3em .15em .5em',
         backgroundColor: '#fad6e7',
         border: '0',
         borderBottom: '1px solid',
