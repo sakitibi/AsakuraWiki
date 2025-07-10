74cef27150b20d27e1f8b32dd3610fc45062f270
sakitibi <sakitibi10nin@icloud.com>
2025-07-10 18:06:38 +0900

    コミットされました

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from 'react'
-import Link from 'next/link'
-import { createClient } from '@supabase/supabase-js'
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import Head from 'next/head';
+import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -67,71 +68,74 @@ export default function Home() {
 
     return (
         <>
-        <main style={{ padding: '2rem' }}>
-            <h1>AsakuraWiki</h1>
-            {loading ? (
-            <p>Loading...</p>
-            ) : pages.length === 0 ? (
-            <p>まだページがありません。</p>
-            ) : (
-            <ul>
-                {pages.map((wp) => (
-                <li key={`${wp.wikiSlug}/${wp.pageSlug}`}>
-                    <Link
-                    href={`/wiki/${wp.wikiSlug}/${wp.pageSlug || 'FrontPage'}`}
-                    >
-                    <button>
-                        <span>
-                            <strong>{wp.name} Wiki*</strong>
-                        </span>
-                    </button>
-                    </Link>{' '}
-                    <small>
-                    （{new Date(wp.updated_at).toLocaleString()}）
-                    </small>
-                </li>
-                ))}
-            </ul>
-            )}
+            <Head>
+                <meta name="google-site-verification" content="MmpT0kkr9zsaqTFT71vXz7Ji13ujnC_kX_0S57tD_Dk" />
+            </Head>
+            <main style={{ padding: '2rem' }}>
+                <h1>AsakuraWiki</h1>
+                {loading ? (
+                <p>Loading...</p>
+                ) : pages.length === 0 ? (
+                <p>まだページがありません。</p>
+                ) : (
+                <ul>
+                    {pages.map((wp) => (
+                    <li key={`${wp.wikiSlug}/${wp.pageSlug}`}>
+                        <Link
+                        href={`/wiki/${wp.wikiSlug}/${wp.pageSlug || 'FrontPage'}`}
+                        >
+                        <button>
+                            <span>
+                                <strong>{wp.name} Wiki*</strong>
+                            </span>
+                        </button>
+                        </Link>{' '}
+                        <small>
+                        （{new Date(wp.updated_at).toLocaleString()}）
+                        </small>
+                    </li>
+                    ))}
+                </ul>
+                )}
 
-            <br />
-            <button onClick={goCreateWiki}>
-                <span>
-                    ＋ 新しいWikiを作る
-                </span>
-            </button>
-        </main>
+                <br />
+                <button onClick={goCreateWiki}>
+                    <span>
+                        ＋ 新しいWikiを作る
+                    </span>
+                </button>
+            </main>
 
-        <footer>
-            <nav id="menu">
-            <ul>
-                <li>
-                    <Link href="/">
-                        <button><span>ホーム</span></button>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/about">
-                        <button><span>当レンタルWikiについて</span></button>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="https://sakitibi.github.io/selects/e38182e38195e382afe383a957696b69">
-                        <button><span>ログイン/新規登録</span></button>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/ru">
-                        <button><span>ロシア語</span></button>
-                    </Link>
-                </li>
-            </ul>
-            </nav>
-            <div style={{ textAlign: 'center', marginTop: '1rem' }}>
-            <p>Copyright 2025 13ninstudio All rights Reserved</p>
-            <p>当Wikiサービスはオープンソースプロジェクトです</p>
-            </div>
-        </footer>
+            <footer>
+                <nav id="menu">
+                <ul>
+                    <li>
+                        <Link href="/">
+                            <button><span>ホーム</span></button>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/about">
+                            <button><span>当レンタルWikiについて</span></button>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="https://sakitibi.github.io/selects/e38182e38195e382afe383a957696b69">
+                            <button><span>ログイン/新規登録</span></button>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/ru">
+                            <button><span>ロシア語</span></button>
+                        </Link>
+                    </li>
+                </ul>
+                </nav>
+                <div style={{ textAlign: 'center', marginTop: '1rem' }}>
+                <p>Copyright 2025 13ninstudio All rights Reserved</p>
+                <p>当Wikiサービスはオープンソースプロジェクトです</p>
+                </div>
+            </footer>
         </>
     )
 }
