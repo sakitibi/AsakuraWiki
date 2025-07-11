c4d8f53c4dc4b027b42057b11da4198e8e812086
sakitibi <sakitibi10nin@icloud.com>
2025-07-11 17:05:44 +0900

    コミットされました

--- components/SelRow.tsx
+++ components/SelRow.tsx
@@ -1,13 +1,5 @@
 import React from 'react';
 
-const containerStyle: React.CSSProperties = {
-    border: '1px solid #ccc',
-    borderRadius: '8px',
-    padding: '1em',
-    background: '#f5f5f5',
-    marginBottom: '1.5em',
-};
-
 export default function SelRow({ children }: { children: React.ReactNode }) {
     return <tr className="sel-row">{children}</tr>;
 }
