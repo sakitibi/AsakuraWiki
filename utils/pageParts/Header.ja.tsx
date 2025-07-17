7ed75a654e2c4093abe39fee6295f3d2a1cd0cb0
sakitibi <sakitibi10nin@icloud.com>
2025-07-17 18:53:40 +0900

    コミットされました

--- utils/pageParts/Header.ja.tsx
+++ utils/pageParts/Header.ja.tsx
@@ -1,30 +1,30 @@
+import { useState } from 'react';
 import styles from 'css/index.min.module.css';
 
 export default function HeaderJa(){
-    let MenuStatus:boolean = false;
-    const MenuOpen = () => {
-        if(MenuStatus === false){
-            MenuStatus = true;
-        } else {
-            MenuStatus = false;
-        }
-    }
-    return (
-        <header className={styles.lHeader}>
-            <div className={styles.container}>
-                <div className={`${styles.row}${styles.alignItemsCenter}${styles.justifyContentBetween}${styles.flexNowrap}`}>
-                    <div className={`${styles.col4}${styles.offset3}`}>
-                        <div className={styles.lHeader__controls}>
-                            <div className={styles.dBlock}>
-                                <div className={styles.cMobileControls}>
-                                    <div className={styles.row}>
-                                        <div className={styles.col6}></div>
-                                        <div className={`${styles.offset1}${styles.col4}`}>
-                                            <button id="menu-button" aria-expanded={MenuStatus} aria-controls="sp-nav" onClick={MenuOpen}>
-                                                <span className={`${styles.cMobileControls__icon}${styles.cMobileControls__iconMenu}`}>
-                                                    <img src="https://wikiwiki.jp/pa/img/icon-menu-white.png" alt="メニュー" width="30" height="30"/>
-                                                </span>
-                                            </button>
+    const MainComponent = () => {
+        const [menuStatus, setMenuStatus] = useState(false);
+
+        const handleMenuOpen = () => {
+            setMenuStatus((prevStatus) => !prevStatus);
+        };
+        return (
+            <header className={styles.lHeader}>
+                <div className={styles.container}>
+                    <div className={`${styles.row}${styles.alignItemsCenter}${styles.justifyContentBetween}${styles.flexNowrap}`}>
+                        <div className={`${styles.col4}${styles.offset3}`}>
+                            <div className={styles.lHeader__controls}>
+                                <div className={styles.dBlock}>
+                                    <div className={styles.cMobileControls}>
+                                        <div className={styles.row}>
+                                            <div className={styles.col6}></div>
+                                            <div className={`${styles.offset1}${styles.col4}`}>
+                                                <button id="menu-button" aria-expanded={menuStatus} aria-controls="sp-nav" onClick={handleMenuOpen}>
+                                                    <span className={`${styles.cMobileControls__icon}${styles.cMobileControls__iconMenu}`}>
+                                                        <img src="https://wikiwiki.jp/pa/img/icon-menu-white.png" alt="メニュー" width="30" height="30"/>
+                                                    </span>
+                                                </button>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -32,7 +32,8 @@ export default function HeaderJa(){
                         </div>
                     </div>
                 </div>
-            </div>
-        </header>
-    )
+            </header>
+        )
+    }
+    return { MainComponent };
 }
