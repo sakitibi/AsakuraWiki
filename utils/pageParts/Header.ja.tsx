356e98248fb62a7cffd46d0af193d8f81ee0820c
sakitibi <sakitibi10nin@icloud.com>
2025-07-17 18:58:59 +0900

    コミットされました

--- utils/pageParts/Header.ja.tsx
+++ utils/pageParts/Header.ja.tsx
@@ -2,25 +2,24 @@ import { useState } from 'react';
 import styles from 'css/index.min.module.css';
 
 export default function HeaderJa(){
-    const MainComponent = () => {
-        const [menuStatus, setMenuStatus] = useState(false);
+    const [menuStatus, setMenuStatus] = useState<boolean>(false);
 
-        const handleMenuOpen = () => {
-            setMenuStatus((prevStatus) => !prevStatus);
-        };
-        return (
+    const handleMenuOpen = () => {
+        setMenuStatus((prevStatus) => !prevStatus);
+    };
+    return (
             <header className={styles.lHeader}>
                 <div className={styles.container}>
-                    <div className={`${styles.row}${styles.alignItemsCenter}${styles.justifyContentBetween}${styles.flexNowrap}`}>
-                        <div className={`${styles.col4}${styles.offset3}`}>
+                    <div className={`${styles.row} ${styles.alignItemsCenter} ${styles.justifyContentBetween} ${styles.flexNowrap}`}>
+                        <div className={`${styles.col4} ${styles.offset3}`}>
                             <div className={styles.lHeader__controls}>
                                 <div className={styles.dBlock}>
                                     <div className={styles.cMobileControls}>
                                         <div className={styles.row}>
                                             <div className={styles.col6}></div>
-                                            <div className={`${styles.offset1}${styles.col4}`}>
-                                                <button id="menu-button" aria-expanded={menuStatus} aria-controls="sp-nav" onClick={handleMenuOpen}>
-                                                    <span className={`${styles.cMobileControls__icon}${styles.cMobileControls__iconMenu}`}>
+                                            <div className={`${styles.offset1} ${styles.col4}`}>
+                                                <button id="menu-button" onClick={handleMenuOpen}>
+                                                    <span className={`${styles.cMobileControls__icon} ${styles.cMobileControls__iconMenu}`}>
                                                         <img src="https://wikiwiki.jp/pa/img/icon-menu-white.png" alt="メニュー" width="30" height="30"/>
                                                     </span>
                                                 </button>
@@ -34,6 +33,4 @@ export default function HeaderJa(){
                 </div>
             </header>
         )
-    }
-    return { MainComponent };
 }
