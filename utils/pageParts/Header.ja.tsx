b7dd07ee8d0f36d5fd198cdf1d7e601bc8833ed4
sakitibi <sakitibi10nin@icloud.com>
2025-07-17 18:31:03 +0900

    コミットされました

--- utils/pageParts/Header.ja.tsx
+++ utils/pageParts/Header.ja.tsx
@@ -7,16 +7,15 @@ export default function HeaderJa(){
                 <div className={`${styles.row}${styles.alignItemsCenter}${styles.justifyContentBetween}${styles.flexNowrap}`}>
                     <div className={`${styles.col4}${styles.offset3}${styles.colLg4}${styles.offsetLg4}`}>
                         <div className={styles.lHeader__controls}>
-                            <div className={styles.DBlock}>
+                            <div className={styles.dBlock}>
                                 <div className={styles.cMobileControls}>
                                     <div className={styles.row}>
                                         <div className={`${styles.offset0}${styles.col6}${styles.colMd4}`}></div>
                                             <div className={`${styles.offset1}${styles.col4}`}>
                                                 <button id="menu-button" aria-expanded="false" aria-controls="sp-nav">
                                                     <span className={`${styles.cMobileControls__icon}${styles.cMobileControls__iconMenu}`}>
-                                                        <img src="https://wikiwiki.jp/pa/img/icon-menu-white.png" alt="メニュー"/>
+                                                        <img src="https://wikiwiki.jp/pa/img/icon-menu-white.png" alt="メニュー" width="30" height="30"/>
                                                     </span>
-                                                    <span className={styles.MobileControls__label}>メニュー</span>
                                                 </button>
                                             </div>
                                     </div>
