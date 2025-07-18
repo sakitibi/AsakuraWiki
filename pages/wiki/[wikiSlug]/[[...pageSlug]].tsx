6aefa7c8ec5680a9c58f67ead4ae4cea27b1257d
sakitibi <sakitibi10nin@icloud.com>
2025-07-18 18:25:23 +0900

    コミットされました

--- pages/wiki/[wikiSlug]/[[...pageSlug]].tsx
+++ pages/wiki/[wikiSlug]/[[...pageSlug]].tsx
@@ -36,7 +36,7 @@ export default function WikiPage() {
     const [editMode, setEditMode] = useState<'private' | 'public'>('public');
     const [designColor, setDesignColor] = useState<'pink' | 'blue' | 'yellow' | null>(null);
     const [showRedirectButton, setShowRedirectButton] = useState(false);
-    const systemMentenans:boolean = true;
+    const systemMentenans:boolean = true; // メンテナンスフラグ
 
     useEffect(() => {
         if (!wikiSlugStr) return;
