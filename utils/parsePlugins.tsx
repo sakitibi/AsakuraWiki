49861fbbbf218f1e2778270dc22402a69c8a34cf
sakitibi <sakitibi10nin@icloud.com>
2025-07-19 14:43:54 +0900

    コミットされました

--- utils/parsePlugins.tsx
+++ utils/parsePlugins.tsx
@@ -180,7 +180,10 @@ export function parseOtherInline(
         else if (token === '#hr') {
             nodes.push(<hr key={key} />)
         }
-        else if (token === '#br' || token === '&br;') {
+        else if (token === '#br') {
+            nodes.push(<br key={key} />)
+        }
+        else if (token === '&br;') {
             nodes.push(<br key={key} />)
         }
         // #ls([title])
