826065bfa39b3f0a25b048b7d4229e18b023a163
sakitibi <sakitibi10nin@icloud.com>
2025-07-20 08:51:28 +0900

    コミットされました

--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -1,39 +1,28 @@
 import Head from 'next/head';
+import { useState } from 'react';
 import styles from 'css/index.min.module.css';
-import Link from 'next/link';
+import MenuJp from '@/utils/pageParts/MenuJp';
+import LeftMenuJp from '@/utils/pageParts/LeftMenuJp';
+import RightMenuJp from '@/utils/pageParts/RightMenuJp';
 
 export default function NewsPage() {
+    const [menuStatus, setMenuStatus] = useState<boolean>(false);
+    const handleClick = () => {
+        setMenuStatus((prevStatus) => {
+            const newStatus = !prevStatus;
+            document.body.style.overflow = newStatus ? 'hidden' : '';
+            return newStatus;
+        });
+    };
     return (
         <>
             <Head>
                 <title>『公式』あさクラニュース!</title>
             </Head>
+            <MenuJp handleClick={handleClick} menuStatus={menuStatus}/>
             <div className={styles.contentsWrapper}>
                 <div className={styles.contents}>
-                    <nav className={styles.menu}>
-                        <ul>
-                            <li>
-                                <Link href="/">
-                                    <button><span>ホーム</span></button>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href="/about">
-                                    <button><span>当レンタルWikiについて</span></button>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href="https://sakitibi.github.io/selects/e38182e38195e382afe383a957696b69">
-                                    <button><span>ログイン/新規登録</span></button>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href="/ru/news">
-                                    <button><span>ロシア語</span></button>
-                                </Link>
-                            </li>
-                        </ul>
-                    </nav>
+                    <LeftMenuJp URL="/news"/>
                     <main style={{ padding: '2rem', flex: 1 }}>
                         <h1>『公式』あさクラニュース!</h1>
                         <ul>
@@ -44,16 +33,7 @@ export default function NewsPage() {
                             注意 ここに出て来るものはあさクラ内の話です、<br/>実際のニュースとは関係無いものもあります、
                         </small>
                     </main>
-                    <aside className={`${styles.lContents__aside} ${styles.childrenSpaced}`}>
-                        <div className={styles.pForBeginner}>
-                            <h2 className={styles.pForBeginner__title}>初めての方へ</h2>
-                            <ul className={styles.pForBeginner__list}>
-                                <li className={styles.pForBeginner__item}>
-                                    <a href="/wiki/sample">サンプルWiki</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </aside>
+                    <RightMenuJp/>
                 </div>
                 <footer className={styles.footer}>
                     <div style={{ textAlign: 'center', marginTop: '1rem' }}>
