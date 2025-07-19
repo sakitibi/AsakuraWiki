630a0a69dd6778e860fe669e5a115e4a1a47c85e
sakitibi <sakitibi10nin@icloud.com>
2025-07-19 17:20:52 +0900

    コミットされました

--- lib/gtag.ts
+++
@@ -1,9 +0,0 @@
-export const GA_ID = process.env.NEXT_PUBLIC_GA_ID || '';
-
-// ページビューの送信
-export const pageview = (url: string) => {
-    if (!GA_ID) return;
-    window.gtag('config', GA_ID, {
-        page_path: url,
-    });
-};
