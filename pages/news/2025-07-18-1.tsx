461026b3d31ccbcc50cd48b7be81095e790421cb
sakitibi <sakitibi10nin@icloud.com>
2025-07-20 08:59:54 +0900

    コミットされました

--- pages/news/2025-07-18-1.tsx
+++ pages/news/2025-07-18-1.tsx
@@ -1,55 +1,35 @@
 import Head from 'next/head';
 import styles from 'css/index.min.module.css';
-import Link from 'next/link';
+import LeftMenuJp from '@/utils/pageParts/LeftMenuJp';
+import MenuJp from '@/utils/pageParts/MenuJp';
+import RightMenuJp from '@/utils/pageParts/RightMenuJp';
+import { useState } from 'react';
 
 export default function NewsPage() {
+    const [menuStatus, setMenuStatus] = useState<boolean>(false);
+    const handleClick = () => {
+        setMenuStatus((prevStatus) => {
+            const newStatus = !prevStatus;
+            document.body.style.overflow = newStatus ? 'hidden' : '';
+            return newStatus;
+        });
+    };
     return (
         <>
             <Head>
                 <title>2025/07/18 あさクラ会議がシステムトラブルで延期に..</title>
             </Head>
+            <MenuJp handleClick={handleClick} menuStatus={menuStatus}/>
             <div className={styles.contentsWrapper}>
                 <div className={styles.contents}>
-                    <nav className={styles.menu}>
-                        <ul>
-                            <li>
-                                <Link href="/">
-                                    <button><span>ホーム</span></button>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href="/about">
-                                    <button><span>当レンタルWikiについて</span></button>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href="https://sakitibi.github.io/selects/e38182e38195e382afe383a957696b69">
-                                    <button><span>ログイン/新規登録</span></button>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href="/ru/news/2025-07-18-1">
-                                    <button><span>ロシア語</span></button>
-                                </Link>
-                            </li>
-                        </ul>
-                    </nav>
+                    <LeftMenuJp URL='/news/2025-07-18-1'/>
                     <main style={{ padding: '2rem', flex: 1 }}>
                         <h1>2025/07/18 あさクラ会議がシステム<br/>システムトラブルで延期に..</h1>
                         <p>その原因は「supabaseのcommentsテーブル<br/>の設定がおかしかったからです」</p>
                         <p>あさクラ会議は延期となり、<br/>すぐにあさクラWikiのシステム<br/>メンテナンスをすることになった</p>
                         <p>現在は修正されています</p>
                     </main>
-                    <aside className={`${styles.lContents__aside} ${styles.childrenSpaced}`}>
-                        <div className={styles.pForBeginner}>
-                            <h2 className={styles.pForBeginner__title}>初めての方へ</h2>
-                            <ul className={styles.pForBeginner__list}>
-                                <li className={styles.pForBeginner__item}>
-                                    <a href="/wiki/sample">サンプルWiki</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </aside>
+                    <RightMenuJp/>
                 </div>
                 <footer className={styles.footer}>
                     <div style={{ textAlign: 'center', marginTop: '1rem' }}>
