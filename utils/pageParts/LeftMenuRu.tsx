0ca3ffb9c5ff6a4524c75adb71d3bba86b821082
sakitibi <sakitibi10nin@icloud.com>
2025-07-20 09:08:00 +0900

    コミットされました

--- utils/pageParts/LeftMenuRu.tsx
+++ utils/pageParts/LeftMenuRu.tsx
@@ -21,13 +21,18 @@ export default function LeftMenuRu({ URL }: Props){
                         </Link>
                     </li>
                     <li>
+                        <Link href="/ru/news">
+                            <button><span>Новости Асакуры</span></button>
+                        </Link>
+                    </li>
+                    <li>
                         <Link href="https://sakitibi.github.io/selects/e38182e38195e382afe383a957696b69">
                             <button><span>Войти/Зарегистрироваться (японский)</span></button>
                         </Link>
                     </li>
                     <li>
                         <Link href={URL}>
-                            <button><span>日本語</span></button>
+                            <button><span>японский</span></button>
                         </Link>
                     </li>
                 </ul>
