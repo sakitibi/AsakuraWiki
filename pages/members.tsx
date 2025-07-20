45b2d9e3de36c95030225a9f17eba0d338872820
sakitibi <sakitibi10nin@icloud.com>
2025-07-20 22:22:34 +0900

    コミットされました

--- pages/members.tsx
+++ pages/members.tsx
@@ -29,6 +29,40 @@ export default function Home() {
                     <LeftMenuJp URL="/members"/>
                     <main style={{ padding: '2rem', flex: 1 }}>
                         <h1>あさクラメンバー一覧</h1>
+                        <ul>
+                            <li><a href="https://youtube.com/channel/UCJcP2mfDCtKnADrbDDjT_8g/?sub_confirmation=1">さきちび(管理者)</a></li>
+                            <ul>
+                                <li><a href="https://youtube.com/@otibinominecraft">つばきちゃん</a></li>
+                                <li><a href="https://youtube.com/@otibinominecraft">つばきくん</a></li>
+                                <li><a href="https://youtube.com/@otibinominecraft">ちびちゃん</a></li>
+                            </ul>
+                            <li><a href="https://youtube.com/@SHIMAJIROCH">あげるくん(副管理者)</a></li>
+                            <ul>
+                                <li><a href='https://youtube.com/@SHIMAJIROCH'>あげるちゃん</a></li>
+                                <li><a href='https://youtube.com/@SHIMAJIROCH'>あみりい</a></li>
+                            </ul>
+                            <li><a href="https://youtube.com/@HikakinGames">ヒカキン</a></li>
+                            <li><a href="https://youtube.com/@28Games28">28(ふたば)</a></li>
+                            <li><a href='https://youtube.com/@72なつちゃんねる'>72(なつ)</a></li>
+                            <li><a href='https://youtube.com/@tiropino'>ちろぴの</a></li>
+                            <li><a href='https://youtube.com/@mkgamech'>MK</a></li>
+                            <li><a href='https://youtube.com/@Memento-mori'>めめんともり</a></li>
+                            <li><a href="https://youtube.com/@Latte1729">Latte</a></li>
+                            <li><a href="https://youtube.com/@upapalon25">ウパパロン</a></li>
+                            <li><a href="https://youtube.com/@saegusarin_0930">三枝りん</a></li>
+                            <li><a href='https://youtube.com/@Mizore471'>みぞれ</a></li>
+                            <li><a href="https://youtube.com/@HinaRuka21">ひなにい</a></li>
+                            <li><a href="https://youtube.com/@S_Reimari">Sレイマリ</a></li>
+                            <li><a href="https://youtube.com/@zennkopasu">ぜんこぱす</a></li>
+                            <li><a href='https://youtube.com/@meteor_s7'>メテヲ</a></li>
+                            <li><a href="https://youtube.com/@HachimanD">八幡宮</a></li>
+                            <li><a href="https://youtube.com/@GGunmas">ガンマス</a></li>
+                            <li><a href="https://youtube.com/@iemon2353">iemon</a></li>
+                            <li><a href="https://youtube.com/@laloryukkuri">レイラー</a></li>
+                            <li><a href="https://youtube.com/@anakena.M">アナケナ・ファミリー</a></li>
+                            <li><a href="https://youtube.com/@chaaaco">茶子</a></li>
+                            <li><a href="https://discord.gg/zbvXxCWcg6">匿名</a></li>
+                        </ul>
                     </main>
                     <RightMenuJp/>
                 </div>
