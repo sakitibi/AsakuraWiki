4c56c42d4e650480fa69348f77bf6f2efa2f537d
sakitibi <sakitibi10nin@icloud.com>
2025-07-20 22:25:26 +0900

    コミットされました

--- pages/members.tsx
+++ pages/members.tsx
@@ -30,17 +30,15 @@ export default function Members() {
                     <main style={{ padding: '2rem', flex: 1 }}>
                         <h1>あさクラメンバー一覧</h1>
                         <ul>
-                            <li><a href="https://youtube.com/channel/UCJcP2mfDCtKnADrbDDjT_8g/?sub_confirmation=1">さきちび(管理者)</a></li>
-                            <ul>
+                            <li><a href="https://youtube.com/channel/UCJcP2mfDCtKnADrbDDjT_8g/?sub_confirmation=1">さきちび(管理者)</a>
                                 <li><a href="https://youtube.com/@otibinominecraft">つばきちゃん</a></li>
                                 <li><a href="https://youtube.com/@otibinominecraft">つばきくん</a></li>
                                 <li><a href="https://youtube.com/@otibinominecraft">ちびちゃん</a></li>
-                            </ul>
-                            <li><a href="https://youtube.com/@SHIMAJIROCH">あげるくん(副管理者)</a></li>
-                            <ul>
+                            </li>
+                            <li><a href="https://youtube.com/@SHIMAJIROCH">あげるくん(副管理者)</a>
                                 <li><a href='https://youtube.com/@SHIMAJIROCH'>あげるちゃん</a></li>
                                 <li><a href='https://youtube.com/@SHIMAJIROCH'>あみりい</a></li>
-                            </ul>
+                            </li>
                             <li><a href="https://youtube.com/@HikakinGames">ヒカキン</a></li>
                             <li><a href="https://youtube.com/@28Games28">28(ふたば)</a></li>
                             <li><a href='https://youtube.com/@72なつちゃんねる'>72(なつ)</a></li>
