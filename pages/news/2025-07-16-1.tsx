6f2740cb8a4f7151a95fd8152f4fc664f4ec0977
sakitibi <sakitibi10nin@icloud.com>
2025-07-20 09:01:49 +0900

    コミットされました

--- pages/news/2025-07-16-1.tsx
+++ pages/news/2025-07-16-1.tsx
@@ -1,39 +1,28 @@
 import Head from 'next/head';
 import styles from 'css/index.min.module.css';
-import Link from 'next/link';
+import MenuJp from '@/utils/pageParts/MenuJp';
+import LeftMenuJp from '@/utils/pageParts/LeftMenuJp';
+import RightMenuJp from '@/utils/pageParts/RightMenuJp';
+import { useState } from 'react';
 
 export default function NewsPage() {
+    const [menuStatus, setMenuStatus] = useState<boolean>(false);
+    const handleClick = () => {
+        setMenuStatus((prevStatus) => {
+            const newStatus = !prevStatus;
+            document.body.style.overflow = newStatus ? 'hidden' : '';
+            return newStatus;
+        });
+    };
     return (
         <>
             <Head>
                 <title>2025/07/16 あさクラ南東部で強風被害が出ています、ご注意下さい</title>
             </Head>
+            <MenuJp handleClick={handleClick} menuStatus={menuStatus}/>
             <div className={styles.contentsWrapper}>
                 <div className={styles.contents}>
-                    <nav className={styles.menu}>
-                        <ul>
-                            <li>
-                                <Link href="/">
-                                    <button><span>ホーム</span></button>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href="/about">
-                                    <button><span>当レンタルWikiについて</span></button>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href="https://sakitibi.github.io/selects/e38182e38195e382afe383a957696b69">
-                                    <button><span>ログイン/新規登録</span></button>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href="/ru/news/2025-07-16-1">
-                                    <button><span>ロシア語</span></button>
-                                </Link>
-                            </li>
-                        </ul>
-                    </nav>
+                    <LeftMenuJp URL='/news/2025-07-16-1'/>
                     <main style={{ padding: '2rem', flex: 1 }}>
                         <h1>2025/07/16 あさクラ南東部で<br/>強風被害が出ています、ご注意下さい</h1>
                         <p>2025/07/15 16:00ごろ Train Builderワールド近海で<br/>強風が発生いたしました</p>
@@ -45,16 +34,7 @@ export default function NewsPage() {
                             <img alt="進路" src="https://sakitibi.github.io/AsakuraWiki-Images/2025-07-16%2015.34.00.png" width="500" height="350"/>
                         </div>
                     </main>
-                    <aside className={`${styles.lContents__aside} ${styles.childrenSpaced}`}>
-                        <div className={styles.pForBeginner}>
-                            <h2 className={styles.pForBeginner__title}>初めての方へ</h2>
-                            <ul className={styles.pForBeginner__list}>
-                                <li className={styles.pForBeginner__item}>
-                                    <a href="/wiki/sample">サンプルWiki</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </aside>
+                    <RightMenuJp/>
                 </div>
                 <footer className={styles.footer}>
                     <div style={{ textAlign: 'center', marginTop: '1rem' }}>
