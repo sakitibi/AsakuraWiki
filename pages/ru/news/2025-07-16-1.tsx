473c53b826f4937a396112ff1270bc4a0a5ca19d
sakitibi <sakitibi10nin@icloud.com>
2025-07-20 09:16:31 +0900

    コミットされました

--- pages/ru/news/2025-07-16-1.tsx
+++ pages/ru/news/2025-07-16-1.tsx
@@ -1,41 +1,28 @@
 import Head from 'next/head';
 import styles from 'css/index.min.module.css';
-import Link from 'next/link';
+import { useState } from 'react';
+import MenuRu from '@/utils/pageParts/MenuRu';
+import LeftMenuRu from '@/utils/pageParts/LeftMenuRu';
+import RightMenuRu from '@/utils/pageParts/RightMenuRu';
 
 export default function NewsPage() {
+    const [menuStatus, setMenuStatus] = useState<boolean>(false);
+    const handleClick = () => {
+        setMenuStatus((prevStatus) => {
+            const newStatus = !prevStatus;
+            document.body.style.overflow = newStatus ? 'hidden' : '';
+            return newStatus;
+        });
+    };
     return (
         <>
             <Head>
                 <title>2025/07/16 Сильный ветер нанес ущерб юго-востоку Асакуры , будьте осторожны.</title>
             </Head>
+            <LeftMenuRu URL='/news/2025-07-16-1'/>
             <div className={styles.contentsWrapper}>
                 <div className={styles.contents}>
-                    <div id="menu">
-                        <nav className={styles.menu}>
-                            <ul>
-                                <li>
-                                <Link href="/ru">
-                                    <button><span>Дом</span></button>
-                                </Link>
-                                </li>
-                                <li>
-                                <Link href="/ru/about">
-                                    <button><span>Об этом вики-сайте по аренде</span></button>
-                                </Link>
-                                </li>
-                                <li>
-                                <Link href="https://sakitibi.github.io/selects/e38182e38195e382afe383a957696b69">
-                                    <button><span>Войти/Зарегистрироваться (японский)</span></button>
-                                </Link>
-                                </li>
-                                <li>
-                                <Link href="/news/2025-07-16-1">
-                                    <button><span>日本語</span></button>
-                                </Link>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
+                    <MenuRu handleClick={handleClick} menuStatus={menuStatus}/>
                     <main style={{ padding: '2rem', flex: 1 }}>
                         <h1>2025/07/16 Сильный ветер нанес ущерб юго-востоку<br/>Асакуры , будьте осторожны.</h1>
                         <p>Около 16:00 15 июля 2025 года в районе Train<br/>Builder World поднялся сильный ветер .</p>
@@ -47,16 +34,7 @@ export default function NewsPage() {
                             <img alt="Путь следующий" src="https://sakitibi.github.io/AsakuraWiki-Images/2025-07-16%2015.34.00.png" width="500" height="350"/>
                         </div>
                     </main>
-                    <aside className={`${styles.lContents__aside} ${styles.childrenSpaced}`}>
-                        <div className={styles.pForBeginner}>
-                            <h2 className={styles.pForBeginner__title}>Для новичков</h2>
-                            <ul className={styles.pForBeginner__list}>
-                                <li className={styles.pForBeginner__item}>
-                                    <a href="/wiki/sample">Образец Wiki</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </aside>
+                    <RightMenuRu/>
                 </div>
                 <footer className={styles.footer}>
                     <div style={{ textAlign: 'center', marginTop: '1rem' }}>
