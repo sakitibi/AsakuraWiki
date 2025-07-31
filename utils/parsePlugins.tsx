70e34f210670454e2ee8630c91a613b317947ece
sakitibi <sakitibi10nin@icloud.com>
2025-07-31 18:38:02 +0900

    コミットされました

--- utils/parsePlugins.tsx
+++ utils/parsePlugins.tsx
@@ -97,7 +97,7 @@ export function extractBracedBlock(source: string, startIdx: number, braceCount:
         braceClosePreview: source.slice(i - braceCount, i + braceCount),
     });
     return {
-        body: source.slice(startIdx + braceCount, i - braceCount), // ← brace分だけ後退
+        body: source.slice(startIdx + braceCount, i - braceCount),
         end: i
     };
 }
