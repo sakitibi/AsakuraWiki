a84374f748fe37be6e229cfb341d18a0ff5afd4d
sakitibi <sakitibi10nin@icloud.com>
2025-08-01 11:19:56 +0900

    コミットされました

--- pages/login/discord.tsx
+++ pages/login/discord.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { supabase } from '../../lib/supabaseClient';
+import { supabase } from '@/lib/supabaseClient';
 
 export default function DiscordLogin() {
     useEffect(() => {
