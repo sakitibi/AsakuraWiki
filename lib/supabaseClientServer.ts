9d4f9db5f0237a39dcbb0f20cc32a2cd854ab333
sakitibi <sakitibi10nin@icloud.com>
2025-08-02 22:20:55 +0900

    コミットされました

--- lib/supabaseClientServer.ts
+++ lib/supabaseClientServer.ts
@@ -3,5 +3,5 @@ import { createClient } from '@supabase/supabase-js';
 
 export const supabaseServer = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY! // サーバー専用のキー
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! // サーバー専用のキー
 );
