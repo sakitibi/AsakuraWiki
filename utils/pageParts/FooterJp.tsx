4ae187c33be240c506b45437eeb361e6b9e93045
sakitibi <sakitibi10nin@icloud.com>
2025-08-11 14:27:29 +0900

    コミットされました

--- utils/pageParts/FooterJp.tsx
+++ utils/pageParts/FooterJp.tsx
@@ -2,8 +2,9 @@ import styles from 'css/index.min.module.css';
 
 export default function FooterJp(){
     return(
-        <footer className={styles.footer}>
-            <div style={{textAlign: "center"}}>
+        <footer className={styles.lFooter}>
+            <div></div>
+            <div style={{textAlign: "center"}} className={styles.cCopyright}>
                 <p>Copyright 2025 13ninstudio All rights Reserved</p>
                 <p>当Wikiサービスはオープンソースプロジェクトです</p>
             </div>
