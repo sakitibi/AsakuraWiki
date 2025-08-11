e2ce840c2268fe215991cc9dbc8651f50a66b3ce
sakitibi <sakitibi10nin@icloud.com>
2025-08-11 22:40:15 +0900

    コミットされました

--- utils/pageParts/FooterJp.tsx
+++ utils/pageParts/FooterJp.tsx
@@ -3,6 +3,13 @@ import styles from 'css/index.min.module.css';
 export default function FooterJp(){
     return(
         <footer className={styles.lFooter}>
+            <div className="container">
+                <ul className={styles.pFooterNav}>
+                    <li className={styles.pFooterNav__item}>
+                        <a href="/policies">利用規約</a>
+                    </li>
+                </ul>
+            </div>
             <div style={{textAlign: "center"}} className={styles.cCopyright}>
                 <p>Copyright 2025 13ninstudio All rights Reserved</p>
                 <p>当Wikiサービスはオープンソースプロジェクトです</p>
