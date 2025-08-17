110e86617f1200b1e7f2d28ec2f1f38860f3b8c6
sakitibi <sakitibi10nin@icloud.com>
2025-08-17 18:36:01 +0900

    コミットされました

--- utils/user_list.ts
+++ utils/user_list.ts
@@ -1,4 +1,4 @@
-export const notuseUsername:string[] = [
+export const notuseUsername:string[] = [ // 利用不可能なユーザー名リスト
     "名前は長い方が有利",
     "NMNGyuri",
     "熟成牛タン",
@@ -10,6 +10,6 @@ export const notuseUsername:string[] = [
     "秋月"
 ];
 
-export const adminerUserId:string[] = [
+export const adminerUserId:string[] = [ // 管理者のuser_idリスト
     "9d2347a2-6322-4d55-93f1-71d8440e5f32"
 ];
