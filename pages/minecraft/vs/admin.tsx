a4900b2c553f30564955d7d5f9053b1167010810
sakitibi <sakitibi10nin@icloud.com>
2025-08-22 11:09:22 +0900

    コミットされました

---
+++ pages/minecraft/vs/admin.tsx
@@ -0,0 +1,116 @@
+import Head from "next/head"
+import styles from 'css/index.min.module.css';
+import HeaderJp from '@/utils/pageParts/HeaderJp';
+import LeftMenuJp from '@/utils/pageParts/LeftMenuJp';
+import RightMenuJp from '@/utils/pageParts/RightMenuJp';
+import FooterJp from '@/utils/pageParts/FooterJp';
+import MenuJp from '@/utils/pageParts/MenuJp';
+import React, { useState } from "react";
+import { useUser } from "@supabase/auth-helpers-react";
+import { supabaseServer } from "@/lib/supabaseClientServer";
+import { adminerUserId } from "@/utils/user_list";
+
+export default function MinecraftVSAdminer(){
+    const [menuStatus, setMenuStatus] = useState<boolean>(false);
+    const [UserId, setUserId] = useState<any>(null);
+    const [UserName, setUserName] = useState<string | null>(null);
+    const [Teams, setTeams] = useState<'赤' | '青' | '緑' | '黄' | null>(null);
+    const [Score, setScore] = useState<number>(0);
+    const [loading, setLoading] = useState(true);
+    const handleClick = () => {
+        setMenuStatus((prevStatus) => {
+            const newStatus = !prevStatus;
+            document.body.style.overflow = newStatus ? 'hidden' : '';
+            return newStatus;
+        });
+    };
+    const user = useUser();
+    const adminer_user_id_list = adminerUserId.find(value => value === user?.id);
+    const AddUsers = async(e: React.FormEvent) => {
+        e.preventDefault();
+        setLoading(true);
+
+        const { error } = await supabaseServer
+        .from('minecraft_vs')
+        .update({ user_name: UserName, user_id: UserId, team: Teams, score: Score})
+        .eq('user_id', UserId);
+
+        setLoading(false);
+
+        if (error) {
+            alert('更新に失敗しました: ' + error.message);
+        } else {
+            alert('設定を保存しました');
+        }
+    }
+    if (loading) return <p>読み込み中...</p>;
+    return(
+        <>
+            <Head>
+                <title>マイクラバーサス ハッピーガスト スカイバトル! 公式</title>
+            </Head>
+            <MenuJp handleClick={handleClick} menuStatus={menuStatus}/>
+            <div className={styles.contentsWrapper}>
+                <HeaderJp handleClick={handleClick}/>
+                <div className={styles.contents}>
+                    <LeftMenuJp URL="/minecraft/vs"/>
+                    <main style={{ padding: '2rem', flex: 1 }}>
+                        {adminer_user_id_list ? (
+                            <>
+                                <h1>マイクラバーサス ハッピーガスト スカイバトル! 管理画面</h1>
+                                <p>ユーザー追加</p>
+                                <form onSubmit={AddUsers}>
+                                    <label>
+                                        ユーザーid(uuid)
+                                        <input
+                                            type="text"
+                                            required
+                                            onChange={(e) => setUserId(e.target.value)}
+                                        />
+                                    </label>
+                                    <label>
+                                        ユーザー名
+                                        <input
+                                            type="text"
+                                            required
+                                            onChange={(e) => setUserName(e.target.value)}
+                                        />
+                                    </label>
+                                    <label>
+                                        チーム
+                                        <select
+                                            onChange={(e) => setTeams(e.target.value as '赤' | '青' | '緑' | '黄')}
+                                        >
+                                            <option value="赤">赤チーム</option>
+                                            <option value="青">青チーム</option>
+                                            <option value="緑">緑チーム</option>
+                                            <option value="黄">黄チーム</option>
+                                        </select>
+                                    </label>
+                                    <label>
+                                        スコア追加(マイナスで減点)
+                                        <input
+                                            type="number"
+                                            required
+                                            onChange={(e) => setScore(Number(e.target.value))}
+                                        />
+                                    </label>
+                                    <button type="submit">
+                                        <span>ユーザーを追加 編集</span>
+                                    </button>
+                                </form>
+                            </>
+                        ) : (
+                            <>
+                                <h1>403 Forbidden</h1>
+                                <p>あなたにはこのページを表示する権限がありません、</p>
+                            </>
+                        )}
+                    </main>
+                    <RightMenuJp/>
+                </div>
+                <FooterJp/>
+            </div>
+        </>
+    )
+}
