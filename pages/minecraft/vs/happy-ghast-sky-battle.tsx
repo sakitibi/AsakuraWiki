0d3b1a8ce6bb5991a21ba0900abcae7b7a9ea1ff
sakitibi <sakitibi10nin@icloud.com>
2025-08-22 16:44:35 +0900

    コミットされました

--- pages/minecraft/vs/happy-ghast-sky-battle.tsx
+++ pages/minecraft/vs/happy-ghast-sky-battle.tsx
@@ -49,9 +49,9 @@ export default function MinecraftVS(){
             console.error("property error:", type);
             return;
         }
-        if(ruleBookImg > 2){
+        if(ruleBookImg > 2){ // 多少のずれがあるため、> 3じゃなくて> 2にする
             setRuleBookImg(0);
-        } else if(ruleBookImg < 1){
+        } else if(ruleBookImg < 1){ // 多少のずれがあるため、< 0じゃなくて< 1にする
             setRuleBookImg(3);
         }
         const RuleBookImgSource:HTMLImageElement = document.getElementById("rule-book-img")! && document.querySelectorAll("img")[1]!
