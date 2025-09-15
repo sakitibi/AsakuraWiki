3fcc86024f3b531cdcd1752a4c0d2642cc27f8d1
sakitibi <sakitibi10nin@icloud.com>
2025-09-15 17:38:23 +0900

    コミットされました

--- pages/index.tsx
+++ pages/index.tsx
@@ -174,7 +174,7 @@ export default function Home() {
                             <h1>あさクラWiki</h1>
                                 <div id="view-counter">
                                     <p>今日の閲覧数: {wiki13ninstudioCounter?.today ?? 0}</p>
-                                    <p>合計の閲覧数: {wiki13ninstudioCounterTotal ?? 0}</p>
+                                    <p>合計の閲覧数: {wiki13ninstudioCounterTotal ? wiki13ninstudioCounterTotal : 0}</p>
                                     <p>昨日の閲覧数: {wiki13ninstudioCounter?.yesterday ?? 0}</p>
                                     <p>現在の閲覧数: {wiki13ninstudioCounter?.online ?? 0}</p>
                                 </div>
