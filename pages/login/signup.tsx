862db027cb0232aa46beeeea38791709b3a971e4
sakitibi <sakitibi10nin@icloud.com>
2025-09-18 23:49:35 +0900

    コミットされました

--- pages/login/signup.tsx
+++ pages/login/signup.tsx
@@ -10,7 +10,7 @@ export default function SignUpPage() {
     const [loading, setLoading] = useState(false);
     const [errorMsg, setErrorMsg] = useState('');
 
-    const notuseUser_list_found = notuseUsername.find(value => value === username);
+    const notuseUser_list_found = notuseUsername.find(value => username.match(value));
 
     const handleSignUp = async (e: React.FormEvent) => {
         e.preventDefault();
--- utils/version.ts
+++ utils/version.ts
@@ -1,4 +1,4 @@
-const version:string = "2.0.27";
+const version:string = "2.0.28";
 const versions:string[] = [
     `V${version}`,
     `v${version}`,
