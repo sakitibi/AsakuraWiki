8850a25116e3737d2e4007255e3297f6e32e6b83
sakitibi <sakitibi10nin@icloud.com>
2025-09-20 17:14:10 +0900

    コミットされました

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    assetPrefix: "./", // Electron の相対パス読み込み用
     webpack(config, { isServer }) {
         if (isServer) {
             config.externals = [...(config.externals || []), 'isows'];
