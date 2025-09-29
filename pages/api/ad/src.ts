21ceacdb5343e029f6ddcb6e124c61824467c456
sakitibi <sakitibi10nin@icloud.com>
2025-09-29 14:57:14 +0900

    コミットされました

--- pages/api/ad/src.ts
+++ pages/api/ad/src.ts
@@ -5,26 +5,33 @@ export default async function handler(
     res: NextApiResponse
 ) {
     try {
+        // ==== CORS ヘッダー ====
+        res.setHeader("Access-Control-Allow-Origin", "*") // 全て許可（制限したい場合はドメイン指定）
+        res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS")
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type")
+
+        // Preflightリクエスト（OPTIONS）の対応
+        if (req.method === "OPTIONS") {
+            return res.status(200).end()
+        }
         if (req.method === 'GET') {
-            return res.status(200).json(
-                {
-                    "src": [
-                        "S7O5-dFA420",
-                        "3lR7twYgDyU",
-                        "uco7lnMyE60",
-                        "tDSKIli-89E",
-                        "6swStVmu9Is",
-                        "6TMwllJkCDo",
-                        "EYeJk0AQ3J0",
-                        "v0xckWVpW2U",
-                        "7eJTeiG83Uo",
-                        "GS0uYMSRXxc",
-                        "zk_-tCv7bzE",
-                        "e-mqoayfaWk",
-                        "MayOi2iFd6U"
-                    ]
-                }
-            )
+            return res.status(200).json({
+                "src": [
+                    "S7O5-dFA420",
+                    "3lR7twYgDyU",
+                    "uco7lnMyE60",
+                    "tDSKIli-89E",
+                    "6swStVmu9Is",
+                    "6TMwllJkCDo",
+                    "EYeJk0AQ3J0",
+                    "v0xckWVpW2U",
+                    "7eJTeiG83Uo",
+                    "GS0uYMSRXxc",
+                    "zk_-tCv7bzE",
+                    "e-mqoayfaWk",
+                    "MayOi2iFd6U"
+                ]
+            })
         }
         // ======================
         // 許可されていないメソッド
