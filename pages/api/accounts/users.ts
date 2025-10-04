90c1feb5a3577ffde03d27e8819c7c2b9e9ccaba
sakitibi <sakitibi10nin@icloud.com>
2025-10-04 17:09:54 +0900

    コミットされました

--- pages/api/accounts/users.ts
+++ pages/api/accounts/users.ts
@@ -1,6 +1,9 @@
 import { supabaseServer } from '@/lib/supabaseClientServer';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { adminerUserId } from '@/utils/user_list';
+
 const ALLOWED_ORIGINS = ['https://asakura-wiki.vercel.app', 'https://sakitibi.github.io'];
+
 export default async function handler(req:NextApiRequest, res: NextApiResponse) {
     const origin = req.headers.origin;
     if (origin && ALLOWED_ORIGINS.includes(origin)) {
@@ -11,14 +14,26 @@ export default async function handler(req:NextApiRequest, res: NextApiResponse) 
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     if (req.method === 'GET') {
-        // データ取得
-        const { data, error } = await supabaseServer
-        .from('user_metadatas')
-        .select('*')
-
-        if (error) return res.status(500).json({ error: error.message });
-        return res.status(200).json(data);
-
+        // ====== 認証ユーザー取得 ======
+        let userId: string | null = null
+        const authHeader = req.headers.authorization
+        if (authHeader?.startsWith('Bearer ')) {
+            const token = authHeader.split(' ')[1]
+            const { data: { user }, error: authError } = await supabaseServer.auth.getUser(token)
+            if (authError) console.error('Supabase auth error:', authError)
+            if (user) userId = user.id
+        }
+        const adminer_user_id_list:boolean = Boolean(adminerUserId.find(value => value === userId));
+        if(adminer_user_id_list){
+            // データ取得
+            const { data, error } = await supabaseServer
+            .from('user_metadatas')
+            .select('*')
+            if (error) return res.status(500).json({ error: error.message });
+            return res.status(200).json(data);
+        } else {
+            return res.status(403).json({ error: "You do not have permission to GET"});
+        }
     } else if (req.method === 'POST') {
         const { metadatas: dataArray } = req.body; // ここで配列を受け取る
         if (!Array.isArray(dataArray) || dataArray.length === 0) {
