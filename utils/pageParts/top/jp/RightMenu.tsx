d8e075f7fdc363fcb54db187df7b3e967d0c1fb4
sakitibi <sakitibi10nin@icloud.com>
2025-10-07 22:22:48 +0900

    コミットされました

--- utils/pageParts/top/jp/RightMenu.tsx
+++ utils/pageParts/top/jp/RightMenu.tsx
@@ -1,6 +1,6 @@
 import styles from 'css/index.min.module.css';
 
-export default function Rightjp/Menu(){
+export default function RightMenuJp(){
     return(
         <aside className={`${styles.lContents__aside} ${styles.childrenSpaced}`}>
             <div className={styles.pForBeginner}>
