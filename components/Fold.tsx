9046efb86a170849506f485fb091a3dba04e8544
sakitibi <sakitibi10nin@icloud.com>
2025-10-08 23:12:42 +0900

    コミットされました

--- components/Fold.tsx
+++ components/Fold.tsx
@@ -103,9 +103,9 @@ export default function Fold({ title, initiallyOpen, children }: FoldProps){
         ? "M64 64C46.3 64 32 78.3 32 96l0 320c0 17.7 14.3 32 32 32l320 0c17.7 0 32-14.3 32-32l0-320c0-17.7-14.3-32-32-32L64 64zM0 96C0 60.7 28.7 32 64 32l320 0c35.3 0 64 28.7 64 64l0 320c0 35.3-28.7 64-64 64L64 480c-35.3 0-64-28.7-64-64L0 96zM128 240l192 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-192 0c-8.8 0-16-7.2-16-16s7.2-16 16-16z"
         : "M64 64C46.3 64 32 78.3 32 96l0 320c0 17.7 14.3 32 32 32l320 0c17.7 0 32-14.3 32-32l0-320c0-17.7-14.3-32-32-32L64 64zM0 96C0 60.7 28.7 32 64 32l320 0c35.3 0 64 28.7 64 64l0 320c0 35.3-28.7 64-64 64L64 480c-35.3 0-64-28.7-64-64L0 96zM208 352l0-80-80 0c-8.8 0-16-7.2-16-16s7.2-16 16-16l80 0 0-80c0-8.8 7.2-16 16-16s16 7.2 16 16l0 80 80 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-80 0 0 80c0 8.8-7.2 16-16 16s-16-7.2-16-16z";
     return(
-        <div style={{ margin: '8px 0' }}>
-            <button onClick={() => setOpen(!open)} type="button" style={{
-                backgroundColor: 'initial',
+        <div style={{ margin: '0px 0px' }}>
+            <button onClick={() => setOpen(!open)} type="button" className="fold-button" style={{
+                backgroundColor: 'white',
                 border: 'none',
                 color: 'inherit',
                 cursor: 'pointer',
