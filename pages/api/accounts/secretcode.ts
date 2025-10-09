fe322148119f5a0ea6618efa6f9a706a61412655
sakitibi <sakitibi10nin@icloud.com>
2025-10-09 23:26:26 +0900

    コミットされました

--- pages/api/accounts/secretcode.ts
+++ pages/api/accounts/secretcode.ts
@@ -74,7 +74,7 @@ export default async function handler(req:NextApiRequest, res: NextApiResponse) 
                 return res.status(200).json(data)
             }
         } catch (error) {
-            res.status(500).json({
+            return res.status(500).json({
                 error: 'JWT生成エラー',
                 details: String(error)
             });
