41faa4a1a531bcf4237aee3596e088123dbff190
sakitibi <sakitibi10nin@icloud.com>
2025-10-11 18:05:29 +0900

    コミットされました

--- pages/wiki/[wikiSlug]/[[...pageSlug]].tsx
+++ pages/wiki/[wikiSlug]/[[...pageSlug]].tsx
@@ -154,26 +154,6 @@ export default function WikiPage() {
     }, [cmdStr, pageSlugStr, wikiSlugStr, user]);
 
     const isEdit:boolean = cmdStr === 'edit';
-    // プレビュー or 閲覧コンテンツ
-    let commentSubmit: HTMLCollectionOf<Element> | null = null;
-    const CommentSubmitInterval = setInterval(() => {
-        if(typeof document.getElementsByClassName("comment-submit") === 'undefined'){
-            if(typeof document.getElementsByClassName("comment-submit") !== 'undefined'){
-                commentSubmit = document.getElementsByClassName("comment-submit");
-            }
-
-            if ((isEdit) && (location.pathname === `/wiki/${wikiSlugStr}` || pageSlugStr === "FrontPage")) {
-                for(let i = 0; i < commentSubmit!.length; i++){
-                    commentSubmit![i].setAttribute("disabled", "true");
-                }
-            }
-        } else {
-            const ClearInterval = setInterval(() => {
-                clearInterval(CommentSubmitInterval);
-                clearInterval(ClearInterval);
-            }, 1000);
-        }
-    }, 1000);
     useEffect(() => {
         CommentSubmitFunc(
             isEdit,
