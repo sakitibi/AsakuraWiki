34122137679241d62f325a293c55ead048d1b51e
sakitibi <sakitibi10nin@icloud.com>
2025-10-12 23:35:34 +0900

    コミットされました

--- pages/store/details/[appid].tsx
+++ pages/store/details/[appid].tsx
@@ -30,7 +30,7 @@ export default function Store() {
     const [apps, setApps] = useState<AppProps[]>([]);
     const [isSetup, setIsSetup] = useState(false);
     const router:NextRouter = useRouter();
-    const { page: appDetails } = router.query;
+    const { appDetails } = router.query;
     // クエリ→文字列化
     const appDetailsStr:string = Array.isArray(appDetails) ? appDetails.join('/') : appDetails ?? '';
     const handleClick = () => {
@@ -48,6 +48,7 @@ export default function Store() {
 
     useEffect(() => {
         const AppDataFetch = async () => {
+            console.log("appDetails: ", appDetails);
             console.log("appDetailsStr: ", appDetailsStr);
             if(!appDetailsStr) return;
             const res = await fetch("/api/store/details", {
