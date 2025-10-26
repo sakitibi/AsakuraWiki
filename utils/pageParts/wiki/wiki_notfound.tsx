6246560bc35775be3c02cdc8f508dff875a4096a
sakitibi <sakitibi10nin@icloud.com>
2025-10-26 17:09:52 +0900

    コミットされました

--- utils/pageParts/wiki/wiki_notfound.tsx
+++ utils/pageParts/wiki/wiki_notfound.tsx
@@ -6,7 +6,7 @@ export function WikiBanned(){
     return(
         <>
             <Head>
-                <link rel="stylesheet" href="https://sakitibi.github.io/static.asakurawiki.com/css/404.min.css"/>
+                <link rel="stylesheet" href="https://sakitibi.github.io/static.asakurawiki.com/css/404.min.static.css"/>
                 <title>404 Not Found</title>
             </Head>
             <main style={{ padding: '2rem', textAlign: 'center', fontFamily: 'sans-serif' }}>
