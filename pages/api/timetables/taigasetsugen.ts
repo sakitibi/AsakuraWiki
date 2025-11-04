12ee8e336c289d72f053afdc207e204425d0c84e
sakitibi <sakitibi10nin@icloud.com>
2025-11-04 22:51:25 +0900

    コミットされました

--- pages/api/timetables/taigasetsugen.ts
+++ pages/api/timetables/taigasetsugen.ts
@@ -44,7 +44,7 @@ export default function handler(req:NextApiRequest, res: NextApiResponse) {
             pillager_for_yukinami.push(
                 {
                     type: "local",
-                    time: `${i < 22 || i >= 78 ? "0" : ""}${(hour + 4) % 24}:${timeflug[(i + 2) % 4] < 10 ? `0${timeflug[(i + 2) % 4]}` : timeflug[(i + 2) % 4]}`,
+                    time: `${i >= 78 ? "00" : i < 22 ? "0" : ""}${(hour + 4) % 24}:${timeflug[(i + 2) % 4] < 10 ? `0${timeflug[(i + 2) % 4]}` : timeflug[(i + 2) % 4]}`,
                     weekday: true,
                     holiday: true,
                     bound_for: i === 18 ? "mangroverin": "yukinami"
@@ -71,7 +71,7 @@ export default function handler(req:NextApiRequest, res: NextApiResponse) {
             yukinami_for_pillager.push(
                 {
                     type: i % 2 === 0 && (i + 4) % 4 === 0 && i <= 72 ? "rapid" : i % 2 === 1 && i <= 71 ? "rapid" : "local",
-                    time: `${i < 22 || i >= 78 ? "0" : ""}${(hour + 4) % 24}:${timeflug[(i + 2) % 4] < 10 ? `0${timeflug[(i + 2) % 4]}` : timeflug[(i + 2) % 4]}`,
+                    time: `${i >= 78 ? "00" : i < 22 ? "0" : ""}${(hour + 4) % 24}:${timeflug[(i + 2) % 4] < 10 ? `0${timeflug[(i + 2) % 4]}` : timeflug[(i + 2) % 4]}`,
                     weekday: true,
                     holiday: true,
                     bound_for: i % 2 === 0 && (i + 4) % 4 === 0 && i <= 72 ? "daiichi_kyoten" : 
@@ -154,7 +154,7 @@ export default function handler(req:NextApiRequest, res: NextApiResponse) {
                 seisomura_for_hinanii_house.push(
                     {
                         type: "rapid",
-                        time: `${i < 5 || i >= 19 ? "0" : ""}${(i + 5) % 24}:06`,
+                        time: `${i >= 19 ? "00" : i < 5 ? "0" : ""}${(i + 5) % 24}:06`,
                         weekday: true,
                         holiday: true,
                         bound_for: seisomura_for_hinanii_house_fortable[i]
@@ -164,7 +164,7 @@ export default function handler(req:NextApiRequest, res: NextApiResponse) {
                 seisomura_for_hinanii_house.push(
                     {
                         type: "local",
-                        time: `${i < 5 || i >= 19 ? "0" : ""}${(i + 5) % 24}:13`,
+                        time: `${i >= 19 ? "00" : i < 5 ? "0" : ""}${(i + 5) % 24}:13`,
                         weekday: true,
                         holiday: true,
                         bound_for: seisomura_for_hinanii_house_fortable[i]
