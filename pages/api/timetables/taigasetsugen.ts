9a9af171b1263a2b11eb9bbbd655a097ecc3e7d4
sakitibi <sakitibi10nin@icloud.com>
2025-11-04 13:54:01 +0900

    コミットされました

--- pages/api/timetables/taigasetsugen.ts
+++ pages/api/timetables/taigasetsugen.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(req:NextApiRequest, res: NextApiResponse) {
+export default function handler(req:NextApiRequest, res: NextApiResponse) {
     res.setHeader('Access-Control-Allow-Origin', "*");
     res.setHeader('Access-Control-Allow-Methods', 'GET');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
