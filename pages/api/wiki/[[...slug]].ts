f5ce07764db042f0ec35c1770c9e576fdbf2fc6b
sakitibi <sakitibi10nin@icloud.com>
2025-12-01 16:34:44 +0900

    コミットされました

--- pages/api/wiki/[[...slug]].ts
+++ pages/api/wiki/[[...slug]].ts
@@ -5,7 +5,6 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    const origin = req.headers.origin;
     res.setHeader('Access-Control-Allow-Origin', "*");
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
