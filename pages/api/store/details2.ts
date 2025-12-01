c1bbf7817e5ca9fa27189734bdb89bd3112a500e
sakitibi <sakitibi10nin@icloud.com>
2025-12-01 23:54:50 +0900

    コミットされました

---
+++ pages/api/store/details2.ts
@@ -0,0 +1,50 @@
+import { supabaseServer } from '@/lib/supabaseClientServer';
+import { adminerUserId } from '@/utils/user_list';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const ALLOWED_ORIGINS = ['https://asakura-wiki.vercel.app', 'https://sakitibi.github.io'];
+let items:obj[] = [];
+
+export interface obj{
+    pw: string;
+    salt: string;
+    iv: string;
+    iterations: number;
+    tagLength: number;
+    ciphertext: string;
+}
+
+export default async function handler(req:NextApiRequest, res: NextApiResponse) {
+    const origin = req.headers.origin;
+    if (origin && ALLOWED_ORIGINS.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    } else {
+        res.setHeader('Access-Control-Allow-Origin', 'null'); // 許可しない場合
+    }
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'POST') {
+        const body:obj = req.body;
+        items.push(body);
+        return res.status(201).json({body});
+    } else if (req.method === "GET"){
+        // ====== 認証ユーザー取得 ======
+        let userId: string | null = null
+        const authHeader = req.headers.authorization
+        if (authHeader?.startsWith('Bearer ')) {
+            const token = authHeader.split(' ')[1]
+            const { data: { user }, error: authError } = await supabaseServer.auth.getUser(token)
+            if (authError) console.error('Supabase auth error:', authError)
+            if (user) userId = user.id
+        }
+        const adminer_user_id_list:boolean = Boolean(adminerUserId.find(value => value === userId));
+        if(adminer_user_id_list){
+            return res.status(200).json(items);
+        } else {
+            return res.status(403).json({ "error": "forbidden" });
+        }
+    } else {
+        res.setHeader('Allow', ['POST','GET']);
+        return res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+}
