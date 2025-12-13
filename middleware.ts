20a01fbcb30c0ae28a5e4a06c696568b436f3dc6
sakitibi <sakitibi10nin@icloud.com>
2025-12-13 18:42:09 +0900

    Merge remote changes

--- middleware.ts
+++ middleware.ts
@@ -1,36 +1,44 @@
 import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
-export function middleware() {
+export function middleware(_: NextRequest) {
     const nonce = Buffer.from(
         crypto.getRandomValues(new Uint8Array(16))
     ).toString('base64');
+
     const csp = `
         default-src
-            'self'
-            https://asakura-wiki.vercel.app
-            https://sakitibi.github.io;
+        'self'
+        https://asakura-wiki.vercel.app
+        https://sakitibi.github.io;
+
         connect-src
-            'self'
-            https://asakura-wiki.vercel.app
-            https://sakitibi.github.io
-            https://www.googletagmanager.com
-            https://counter.wikiwiki.jp
-            https://gppjfculpjyjqzfuqfev.supabase.co
-            https://ipwho.is;
+        'self'
+        https://asakura-wiki.vercel.app
+        https://sakitibi.github.io
+        https://www.googletagmanager.com
+        https://counter.wikiwiki.jp
+        https://gppjfculpjyjqzfuqfev.supabase.co
+        https://ipwho.is;
+
         script-src
-            'self'
-            https://asakura-wiki.vercel.app
-            https://sakitibi.github.io
-            https://www.googletagmanager.com;
+        'self'
+        'nonce-${nonce}'
+        https://asakura-wiki.vercel.app
+        https://sakitibi.github.io
+        https://www.googletagmanager.com;
+
         style-src
-            'self'
-            https://sakitibi.github.io
-            https://asakura-wiki.vercel.app;
+        'self'
+        'nonce-${nonce}'
+        https://sakitibi.github.io
+        https://asakura-wiki.vercel.app;
+
         img-src
-            'self'
-            https://sakitibi.github.io
-            https://asakura-wiki.vercel.app
-            https://yt3.googleusercontent.com;
+        'self'
+        https://sakitibi.github.io
+        https://asakura-wiki.vercel.app
+        https://yt3.googleusercontent.com;
     `.replace(/\n/g, ' ').trim();
     const res = NextResponse.next();
     res.headers.set('Content-Security-Policy', csp);
--- next.config.js
+++ next.config.js
@@ -1,50 +1,7 @@
 /** @type {import('next').NextConfig} */
-const CSP = `
-    default-src
-        'self'
-        https://asakura-wiki.vercel.app
-        https://sakitibi.github.io;
-    connect-src
-        'self'
-        https://asakura-wiki.vercel.app
-        https://sakitibi.github.io
-        https://www.googletagmanager.com
-        https://counter.wikiwiki.jp
-        https://gppjfculpjyjqzfuqfev.supabase.co
-        https://ipwho.is;
-    script-src
-        'self'
-        https://asakura-wiki.vercel.app
-        https://sakitibi.github.io
-        https://www.googletagmanager.com;
-    style-src
-        'self'
-        https://sakitibi.github.io
-        https://asakura-wiki.vercel.app;
-    img-src
-        'self'
-        https://sakitibi.github.io
-        https://asakura-wiki.vercel.app
-        https://yt3.googleusercontent.com;
-`;
-
 const nextConfig = {
     output: 'standalone',
     turbopack: {},
-
-    async headers() {
-        return [
-            {
-                source: '/(.*)',
-                headers: [
-                {
-                    key: 'Content-Security-Policy',
-                    value: CSP.replace(/\n/g, ' ').trim(),
-                },
-                ],
-            },
-        ];
-    },
 
     webpack(config, { isServer }) {
         if (isServer) {
