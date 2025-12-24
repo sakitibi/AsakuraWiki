bddae01a97602c98628e487927339a7a6588f7af
sakitibi <sakitibi10nin@icloud.com>
2025-12-24 23:09:57 +0900

    Merge remote changes

--- pages/news/2025/12/22/1.tsx
+++ pages/news/2025/12/22/1.tsx
@@ -35,9 +35,9 @@ export default function NewsPage() {
                     <main style={{ padding: '2rem', flex: 1 }}>
                         <h1>2025/12/22 24日〜25日について</h1>
                         <p>12月24日〜25日は</p>
-                        <p>あさクラ全体の鉄道線が大幅に遅れます</p>
+                        <p>あさクラ全体の鉄道線が大幅に遅れました</p>
                         <p>なぜなら名前は長い方が有利への対応に協力する為です</p>
-                        <p style={{ color: "red" }}>{company}の社員たちも休日出勤となります</p>
+                        <p style={{ color: "red" }}>{company}の社員たちも休日出勤となりました</p>
                         <p>利用者に出来ること</p>
                         <ol>
                             <li>SNSなどで名前は長い方が有利を批判する</li>
