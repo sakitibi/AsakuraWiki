cb3430108b1935ffc37578c74c162fa31f47fd37
sakitibi <sakitibi10nin@icloud.com>
2026-01-10 15:44:42 +0900

    Merge remote changes

--- pages/store/details/[appDetails].tsx
+++ pages/store/details/[appDetails].tsx
@@ -72,17 +72,12 @@ export default function Store() {
                 const ripple = document.createElement("span");
                 ripple.className = "ripple";
 
-                const size = Math.max(rect.width, rect.height);
-                ripple.style.width = ripple.style.height = `${size}px`;
-
-                ripple.style.left = `${e.clientX - rect.left - size / 2}px`;
-                ripple.style.top = `${e.clientY - rect.top - size / 2}px`;
+                ripple.style.left = `${e.clientX - rect.left}px`;
+                ripple.style.top  = `${e.clientY - rect.top}px`;
 
                 btn.appendChild(ripple);
 
-                ripple.addEventListener("animationend", () => {
-                    ripple.remove();
-                });
+                ripple.addEventListener("animationend", () => ripple.remove());
             });
         });
     }, [apps]);
