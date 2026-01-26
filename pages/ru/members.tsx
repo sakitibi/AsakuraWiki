ec4e8db442dde11ee36f3ee88b14a35db141baee
sakitibi <sakitibi10nin@icloud.com>
2026-01-26 22:27:16 +0900

    Merge remote changes

--- pages/ru/members.tsx
+++ pages/ru/members.tsx
@@ -33,41 +33,50 @@ export default function Members() {
                     <LeftMenuRu URL="/members"/>
                     <main style={{ padding: '2rem', flex: 1 }}>
                         <h1>Список участников Асакуры</h1>
+                        <p>Текущее количество участников: 32, 5 находятся на рассмотрении.</p>
                         <ul className='list1'>
-                            <li><a href="https://youtube.com/channel/UCJcP2mfDCtKnADrbDDjT_8g/?sub_confirmation=1">Сакичиби (Администратор)</a>
+                            <li><a href="https://youtube.com/channel/UCJcP2mfDCtKnADrbDDjT_8g/?sub_confirmation=1">Сакичиби (Администратор) Присоединился 1 января 2021 года.</a>
                                 <ul className='list2'>
-                                    <li><a href="https://youtube.com/@otibinominecraft">Цубаки-чан</a></li>
-                                    <li><a href="https://youtube.com/@otibinominecraft">Цубаки-кун</a></li>
-                                    <li><a href="https://youtube.com/@otibinominecraft">ЦубакиЦубаки-чан</a></li>
+                                    <li><a href="https://youtube.com/@otibinominecraft">Цубаки-чан Присоединился 1 января 2021 года.</a></li>
+                                    <li><a href="https://youtube.com/@otibinominecraft">Цубаки-кун Присоединился 1 января 2021 года.</a></li>
+                                    <li><a href="https://youtube.com/@otibinominecraft">ЦубакиЦубаки-чан Присоединился 1 января 2021 года.</a></li>
                                 </ul>
                             </li>
-                            <li><a href="https://youtube.com/@SHIMAJIROCH">Агерукун (помощник администратора)</a>
+                            <li><a href="https://youtube.com/@SHIMAJIROCH">Агерукун (помощник администратора) Участвовал 26 января 2023 года.</a>
                                 <ul className='list2'>
-                                    <li><a href='https://youtube.com/@SHIMAJIROCH'>Агеру-чан</a></li>
-                                    <li><a href='https://youtube.com/@SHIMAJIROCH'>Амири</a></li>
+                                    <li><a href='https://youtube.com/@SHIMAJIROCH'>Агеру-чан Участвовал 26 января 2023 года.</a></li>
+                                    <li><a href='https://youtube.com/@SHIMAJIROCH'>Амири Участвовал 26 января 2023 года.</a></li>
                                 </ul>
                             </li>
-                            <li><a href="https://youtube.com/@HikakinGames">Хикакин</a></li>
-                            <li><a href="https://youtube.com/@28Games28">28(Футаба)</a></li>
-                            <li><a href='https://youtube.com/@72なつちゃんねる'>72(Лето)</a></li>
-                            <li><a href='https://youtube.com/@tiropino'>Чиропино</a></li>
-                            <li><a href='https://youtube.com/@mkgamech'>МК</a></li>
-                            <li><a href='https://youtube.com/@Memento-mori'>Мементомори</a></li>
-                            <li><a href="https://youtube.com/@Latte1729">Латте</a></li>
-                            <li><a href="https://youtube.com/@upapalon25">Упапалон</a></li>
-                            <li><a href="https://youtube.com/@gusao_gusarin">Рин Саэгуса</a></li>
-                            <li><a href='https://youtube.com/@Mizore471'>Мокрый снег</a></li>
-                            <li><a href="https://youtube.com/@HinaRuka21">Хинани</a></li>
-                            <li><a href="https://youtube.com/@S_Reimari">С. Реймари</a></li>
-                            <li><a href="https://youtube.com/@zennkopasu">Зенкопасс</a></li>
-                            <li><a href='https://youtube.com/@meteor_s7'>Метеор</a></li>
-                            <li><a href="https://youtube.com/@HachimanD">Святилище Хатимана</a></li>
-                            <li><a href="https://youtube.com/@GGunmas">Гамма</a></li>
-                            <li><a href="https://youtube.com/@iemon2353">кто-то</a></li>
-                            <li><a href="https://youtube.com/@laloryukkuri">Рейлор</a></li>
-                            <li><a href="https://youtube.com/@anakena.M">Семья Анакена</a></li>
-                            <li><a href="https://youtube.com/@chaaaco">Чако</a></li>
-                            <li><a href="https://discord.gg/zbvXxCWcg6">Токумеи</a></li>
+                            <li><a href="https://youtube.com/@HikakinGames">Хикакин Участвовал 1 февраля 2023 года.</a></li>
+                            <li><a href="https://youtube.com/@28Games28">28(Футаба) Участвовал 1 февраля 2023 года.</a></li>
+                            <li><a href='https://youtube.com/@72なつちゃんねる'>72(Лето) Участвовал 1 февраля 2023 года.</a></li>
+                            <li><a href='https://youtube.com/@tiropino'>Чиропино Участвовал 1 февраля 2023 года.</a></li>
+                            <li><a href='https://youtube.com/@mkgamech'>МК Участвовал 1 февраля 2023 года.</a></li>
+                            <li><a href="https://youtube.com/@WaonGames">Waon GAMES / Waon Games Присоединился 1 февраля 2023 г. Ушел 1 августа 2024 г.</a></li>
+                            <li><a href='https://youtube.com/@Memento-mori'>Мементомори Участвовал 1 февраля 2023 года.</a></li>
+                            <li><a href="https://youtube.com/@Latte1729">Латте Участвовал 1 февраля 2023 года.</a></li>
+                            <li><a href="https://youtube.com/@upapalon25">Упапалон Участвовал 1 февраля 2023 года.</a></li>
+                            <li><a href="https://youtube.com/@gusao_gusarin">Рин Саэгуса Участвовал 1 февраля 2023 года.</a></li>
+                            <li><a href='https://youtube.com/@Mizore471'>Мокрый снег Участвовал 1 февраля 2023 года.</a></li>
+                            <li><a href="https://youtube.com/@HinaRuka21">Хинани Участвовал 1 февраля 2023 года.</a></li>
+                            <li><a href="https://youtube.com/@S_Reimari">С. Реймари Участвовал 1 февраля 2023 года.</a></li>
+                            <li><a href="https://youtube.com/@zennkopasu">Зенкопасс Участвовал 1 февраля 2023 года.</a></li>
+                            <li><a href='https://youtube.com/@meteor_s7'>Метеор Участвовал 1 февраля 2023 года.</a></li>
+                            <li><a href="https://youtube.com/@HachimanD">Святилище Хатимана Участвовал 1 февраля 2023 года.</a></li>
+                            <li><a href="https://youtube.com/@GGunmas">Гамма Участвовал 1 февраля 2023 года.</a></li>
+                            <li><a href="https://youtube.com/@iemon2353">кто-то Участвовал 1 февраля 2023 года.</a></li>
+                            <li><a href="https://youtube.com/@laloryukkuri">Рейлор Участвовал 1 февраля 2023 года.</a></li>
+                            <li><a href="https://www.youtube.com/channel/UCybjdcp-MGis0LYEP9kGRFA">Марио (ссылка не работает) Присоединился 1 февраля 2023 г. Ушёл 3 марта 2025 г.</a></li>
+                            <li><a href="https://youtube.com/@anakena.M">Семья Анакена Участвовал 1 марта 2023 года.</a></li>
+                            <li><a href="https://youtube.com/@chaaaco">Чако Участие состоялось 4 марта 2025 года.</a></li>
+                            <li><a href="https://discord.gg/zbvXxCWcg6">Токумеи Присоединился 12 августа 2025 года.</a></li>
+                            <li><a href='https://youtube.com/@yuinel'>Yuineru присоединился 27 января 2026 года.</a></li>
+                            <li>На рассмотрении</li>
+                            <li>На рассмотрении</li>
+                            <li>На рассмотрении</li>
+                            <li>На рассмотрении</li>
+                            <li>На рассмотрении</li>
                         </ul>
                     </main>
                     <RightMenuRu/>
