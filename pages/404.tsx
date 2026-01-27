09880dddf0285711e95da6e679c5fd9ecd437bda
sakitibi <sakitibi10nin@icloud.com>
2026-01-27 23:11:37 +0900

    Merge remote changes

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import FooterJp from '@/utils/pageParts/top/jp/Footer';
+import Link from 'next/link';
 
 interface Custom404Props{
     isEmbed?: "false" | "true"
@@ -20,7 +21,18 @@ export default function Custom404({
             <main style={{ padding: '2rem', textAlign: 'center', fontFamily: 'sans-serif' }}>
                 <div className="article text-center">
                     <h1>404 Not Found</h1>
+                    <p>
+                        <img src="https://sakitibi.github.io/AsakuraWiki-Images/yuinel.png"
+                            width="180"
+                            height="180"
+                        />
+                    </p>
                     <p>お探しのページは存在しません。</p>
+                    <Link href="/">
+                        <button>
+                            <span>トップへ戻る</span>
+                        </button>
+                    </Link>
                 </div>
             </main>
             {isEmbed !== "true" ? <FooterJp/> : null}
