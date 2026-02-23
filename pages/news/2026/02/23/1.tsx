c4531c2ef7e43ac1ee87173326b050b82075063f
さきちび <sakitibi10nin@icloud.com>
2026-02-23 20:02:43 +0900

    Create 1.tsx

---
+++ pages/news/2026/02/23/1.tsx
@@ -0,0 +1,48 @@
+import Head from 'next/head';
+import styles from '@/css/index.module.css';
+import LeftMenuJp from '@/utils/pageParts/top/jp/LeftMenu';
+import MenuJp from '@/utils/pageParts/top/jp/Menu';
+import RightMenuJp from '@/utils/pageParts/top/jp/RightMenu';
+import { useState, useEffect } from 'react';
+import HeaderJp from '@/utils/pageParts/top/jp/Header';
+import FooterJp from '@/utils/pageParts/top/jp/Footer';
+
+
+export default function NewsPage() {
+    const [menuStatus, setMenuStatus] = useState(false);
+    useEffect(() => {
+        if(typeof document !== "undefined"){
+            document.body.style.overflow = menuStatus ? "hidden" : "";
+            return () => {
+                document.body.style.overflow = "";
+            };
+        }
+    }, [menuStatus]);
+    const handleClick = () => {
+        setMenuStatus(prev => !prev);
+    };
+    return (
+        <>
+            <Head>
+                <title>2026/02/23 大事なお知らせ</title>
+            </Head>
+            <MenuJp handleClick={handleClick} menuStatus={menuStatus}/>
+            <div className={styles.contentsWrapper}>
+                <HeaderJp handleClick={handleClick}/>
+                <div className={styles.contents}>
+                    <LeftMenuJp URL="/news/2026/02/23/1"/>
+                    <main style={{ padding: '2rem', flex: 1 }}>
+                        <h1>2026/02/23 大事なお知らせ</h1>
+                        <p>13ninstudioはゲーム事業から完全撤退します</p>
+                        <p>JSObfuscatorも撤退しました</p>
+                        <p>名前は長い方が有利の配信監視も</p>
+                        <p>もう時効が25日に来ます</p>
+                        <p>バージョンはおそらくV2.0.80で凍結されます</p>
+                    </main>
+                    <RightMenuJp/>
+                </div>
+                <FooterJp/>
+            </div>
+        </>
+    )
+}
