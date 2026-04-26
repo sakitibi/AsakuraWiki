50ac2a2db4b73cc0b6ff2d45e0d7044688f20367
sakitibi <sakitibi10nin@icloud.com>
2026-04-26 14:47:12 +0900

    Merge remote changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,37 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
 const flatten = require('gulp-flatten');
-const cleanCSS = require('gulp-clean-css'); // 追加
+const postcss = require('gulp-postcss'); // 追加
+const cssnano = require('cssnano');     // 追加
+const path = require('path');
 
 function compileSass() {
   return gulp.src('scss/**/*.scss')
+    // 1. ソースマップ初期化
     .pipe(sourcemaps.init())
+    
+    // 2. Sassコンパイル (ここでは一旦 expanded でOK)
     .pipe(sass({ 
-      // ここは expanded でも compressed でも OK
       includePaths: ['node_modules'],
       loadPaths: ['node_modules'],
       quietDeps: true 
     }).on('error', sass.logError))
 
-    // 階層をフラットにする
+    // 3. 階層を直下に移動
     .pipe(flatten())
 
-    // ★ 明示的に CSS を圧縮する（これが一番確実です）
-    .pipe(cleanCSS()) 
+    // 4. 【重要】PostCSS(cssnano) で確実に圧縮
+    // sourcemaps.write の直前に入れることで、圧縮後のマップを生成します
+    .pipe(postcss([cssnano()])) 
     
-    // ソースマップを書き出し
-    .pipe(sourcemaps.write('./'))
+    // 5. ソースマップ書き出し
+    .pipe(sourcemaps.write('./', {
+      includeContent: true, 
+      mapSources: (sourcePath) => path.basename(sourcePath),
+      sourceRoot: '../scss'
+    }))
     
+    // 6. 出力
     .pipe(gulp.dest('css'));
 }
 
