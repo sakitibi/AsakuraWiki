ea08a2c3a0d5f722fb5820b226c5e0833fdb3ce6
sakitibi <sakitibi10nin@icloud.com>
2026-04-26 15:01:43 +0900

    Merge remote changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,37 +2,50 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
 const flatten = require('gulp-flatten');
-const postcss = require('gulp-postcss'); // 追加
-const cssnano = require('cssnano');     // 追加
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
 const path = require('path');
+const through2 = require('through2');
 
 function compileSass() {
   return gulp.src('scss/**/*.scss')
-    // 1. ソースマップ初期化
     .pipe(sourcemaps.init())
     
-    // 2. Sassコンパイル (ここでは一旦 expanded でOK)
     .pipe(sass({ 
       includePaths: ['node_modules'],
       loadPaths: ['node_modules'],
       quietDeps: true 
     }).on('error', sass.logError))
 
-    // 3. 階層を直下に移動
-    .pipe(flatten())
+    // 1. 【重要】Sass直後の完璧なマップ情報を「丸ごと」保存する
+    .pipe(through2.obj(function(file, enc, cb) {
+      if (file.sourceMap) {
+        file.savedMap = JSON.parse(JSON.stringify(file.sourceMap));
+      }
+      cb(null, file);
+    }))
 
-    // 4. 【重要】PostCSS(cssnano) で確実に圧縮
-    // sourcemaps.write の直前に入れることで、圧縮後のマップを生成します
+    .pipe(flatten())
     .pipe(postcss([cssnano()])) 
     
-    // 5. ソースマップ書き出し
+    // 2. 【重要】破壊されたマップ情報を、保存しておいた情報で「強制置換」する
+    .pipe(through2.obj(function(file, enc, cb) {
+      if (file.savedMap && file.sourceMap) {
+        // 保存していた sources と sourcesContent を復元
+        file.sourceMap.sources = file.savedMap.sources.map(s => path.basename(s));
+        file.sourceMap.sourcesContent = file.savedMap.sourcesContent;
+        // メインファイル名が .css になっていたら .scss に戻す
+        file.sourceMap.sources = file.sourceMap.sources.map(s => s.replace(/\.css$/, '.scss'));
+      }
+      cb(null, file);
+    }))
+
+    // 3. すでに情報は復元済みなので、そのまま書き出す
     .pipe(sourcemaps.write('./', {
-      includeContent: true, 
-      mapSources: (sourcePath) => path.basename(sourcePath),
+      includeContent: true,
       sourceRoot: '../scss'
     }))
     
-    // 6. 出力
     .pipe(gulp.dest('css'));
 }
 
