30913dd7c4fe22dfae94a567438e2faada4d435e
sakitibi <sakitibi10nin@icloud.com>
2026-05-03 09:14:16 +0900

    Merge remote changes

--- pages/api/amongus/token.ts
+++ pages/api/amongus/token.ts
@@ -1,6 +1,18 @@
 import { supabaseClient } from "@/lib/supabaseClient";
 import type { NextApiRequest, NextApiResponse } from "next";
 
+function isOneHourEarlier(referenceDate: Date) {
+    if (!(referenceDate instanceof Date)) {
+        throw new Error("referenceDate は有効な Date オブジェクトである必要があります");
+    }
+
+    const now = new Date();
+    const diffMs = referenceDate.getTime() - now.getTime(); // 基準 - 現在
+    const oneHourMs = 60 * 60 * 1000;
+
+    return diffMs >= oneHourMs;
+}
+
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
@@ -11,13 +23,14 @@ export default async function handler(
     if (req.method === "GET") {
         const {data, error} = await supabaseClient
             .from("wiki_variables")
-            .select("value")
+            .select("value,updated_at")
             .eq("id", "a7869bcb-1c09-b4b2-4939-d382a5f27247")
             .single()
         if (error) {
             return res.status(500).json({error: error});
         }
-        if (!data.value) {
+        const date = new Date(data.updated_at);
+        if (!data.value || isOneHourEarlier(date)) {
             return res.status(500).json({error: "token is null"});
         }
         return res.status(200).json({token: data.value ?? null})
