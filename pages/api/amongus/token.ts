f0e0a54ec02af7f03db2d50b867371a3e468ae84
sakitibi <sakitibi10nin@icloud.com>
2026-05-03 13:34:25 +0900

    Merge remote changes

--- pages/api/amongus/token.ts
+++ pages/api/amongus/token.ts
@@ -20,6 +20,8 @@ export default async function handler(
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,PUT');
 
+    if (req.method === 'OPTIONS') return res.status(200).end();
+
     if (req.method === "GET") {
         const {data, error} = await supabaseClient
             .from("wiki_variables")
