b1028b14db9cad6d1daaabaf1da4f2536e460cdd
sakitibi <sakitibi10nin@icloud.com>
2026-05-12 00:00:20 +0900

    Merge remote changes

--- pages/special_wiki/13ninstudio/staff_credits.tsx
+++ pages/special_wiki/13ninstudio/staff_credits.tsx
@@ -5,6 +5,12 @@ import { useEffect, useState } from "react";
 
 export default function Redirecting(){
     const [user, setUser] = useState<User | null>(null);
+    const [isBot, setIsBot] = useState(true);
+    useEffect(() => {
+        const ua = navigator.userAgent;
+        const bot = /(Googlebot|Google-InspectionTool|AdsBot-Google|bingbot|Slurp|DuckDuckBot|YandexBot|Baiduspider)/i.test(ua);
+        setIsBot(bot);
+    }, []);
     useEffect(() => {
         supabaseClient.auth.getUser().then(({ data, error }) => {
             console.log('[getUser]', { data, error });
@@ -15,14 +21,18 @@ export default function Redirecting(){
         });
     }, []);
     const Redirect = async () => {
-        if (user) {
-            const session = await supabaseClient.auth.getSession();
-            const token = session?.data?.session?.access_token
-            if(typeof location !== "undefined" && typeof window !== "undefined"){
-                location.replace(`https://sakitibi.github.io/13nin.com/staff_credits?login=${token}`);
-            }
+        if (isBot) {
+            location.replace("https://sakitibi.github.io/13nin.com/staff_credits");
         } else {
-            location.replace("/login");
+            if (user) {
+                const session = await supabaseClient.auth.getSession();
+                const token = session?.data?.session?.access_token
+                if(typeof location !== "undefined" && typeof window !== "undefined"){
+                    location.replace(`https://sakitibi.github.io/13nin.com/staff_credits?login=${token}`);
+                }
+            } else {
+                location.replace("/login");
+            }
         }
     }
     useEffect(() => {
