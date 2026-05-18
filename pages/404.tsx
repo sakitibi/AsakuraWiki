b302aedae739a6096ac595360080d078f409dffb
sakitibi <sakitibi10nin@icloud.com>
2026-05-18 21:52:41 +0900

    Merge remote changes

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import FooterJp from '@/utils/pageParts/top/jp/Footer';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 interface Custom404Props{
     isEmbed?: "false" | "true"
@@ -12,6 +13,12 @@ export default function Custom404({
     if(isEmbed !== "true"){
         isEmbed = "false"
     }
+    const [pathname, setPathname] = useState<string | null>(null);
+    useEffect(() => {
+        if (typeof location !== "undefined") {
+            setPathname(location.pathname);
+        }
+    }, []);
     return (
         <>
             <Head>
@@ -30,7 +37,7 @@ export default function Custom404({
                         />
                     </p>
                     <p>ごめんなさい。</p>
-                    <p>お探しのページ( {location.pathname} )が見つかりませんでした。</p>
+                    <p>お探しのページ( {pathname} )が見つかりませんでした。</p>
                     <Link href="/">
                         <button>
                             <span>トップへ戻る</span>
