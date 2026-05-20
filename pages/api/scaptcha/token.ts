ef8f7d7bc3ec5c2412f28464dc40847ceaa98fdf
sakitibi <sakitibi10nin@icloud.com>
2026-05-21 00:09:40 +0900

    Merge remote changes

---
+++ pages/api/scaptcha/token.ts
@@ -0,0 +1,66 @@
+import { supabaseServer } from "@/lib/supabaseClientServer";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const ALLOWED_ORIGINS = ['https://asakura-wiki.vercel.app', 'https://sakitibi.github.io'];
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    const origin = req.headers.origin;
+    if (origin && ALLOWED_ORIGINS.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    } else {
+        res.setHeader('Access-Control-Allow-Origin', 'null'); // 許可しない場合
+    }
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-scaptcha-session');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+    const scaptcha_params = req.headers["x-scaptcha-session"];
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    } else if (req.method === "GET") {
+        if (!scaptcha_params) {
+            return res.status(401).send("Error 401 Unauthorized");
+        }
+        const { data, error } = await supabaseServer
+            .from("scaptcha_session")
+            .select("*")
+            .eq("id", scaptcha_params)
+            .single();
+        if (error || !data) {
+            return res.status(404).send("Error 404 NotFound");
+        }
+        return res.status(200).json(data);
+    } else if (req.method === "POST") {
+        if (!req.body) {
+            return res.status(401).send("Error 401 Unauthorized");
+        }
+        const { error } = await supabaseServer
+            .from("scaptcha_session")
+            .insert([{
+                data: req.body,
+                created_at: new Date()
+            }])
+            .single();
+        if (error) {
+            return res.status(500).send("Error 500 Internal Server Error");
+        }
+        return res.status(204).end();
+    } else if (req.method === "DELETE") {
+        if (!scaptcha_params) {
+            return res.status(401).send("Error 401 Unauthorized");
+        }
+        const { error } = await supabaseServer
+            .from("scaptcha_session")
+            .delete()
+            .eq("id", scaptcha_params)
+            .single();
+        if (error) {
+            return res.status(500).send("Error 500 Internal Server Error");
+        }
+        return res.status(204).end();
+    } else {
+        res.setHeader('Allow', ['GET','POST','DELETE','OPTIONS']);
+        return res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+}
