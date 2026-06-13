81cc5ad3f78a8bdc2d211bb33026669fe4eaf0d9
sakitibi <sakitibi10nin@icloud.com>
2026-06-13 14:11:52 +0900

    Merge remote changes

--- common.cjs
+++ common.cjs
@@ -4,19 +4,17 @@ const upack = require('./node_modules/upack.js/dist/cjs/index.cjs');
 
 // 調べたいディレクトリの絶対パス
 const dirPath = '/vercel/path0/.next/output/static/_next/static'; 
-//const dirPath = '/vercel/path0/.next/output/static/_next/static/chunks'; 
-
 try {
     const files = fs.readdirSync(dirPath);
-    console.log("files: ", files);
     const filtered = files.filter(value => value !== "chunks" && value !== "not-found.txt");
     const deployedFiles = fs.readdirSync(`${dirPath}/${filtered[0]}`);
-    console.log("deployedFiles: ", deployedFiles);
     const buildManifestFile = new TextDecoder().decode(
         fs.readFileSync(`${dirPath}/${filtered[0]}/_buildManifest.js`)
     );
     const sliced = JSON.parse(buildManifestFile.slice(24, buildManifestFile.length - 55));
-    console.log(sliced);
+    console.log("sliced: ", sliced);
+    const rootBase = fs.readFileSync(`/vercel/path0/.next/output/static/_next/${sliced["/"][0]}`);
+    console.log("rootBase: ", rootBase);
 } catch (err) {
     console.error('エラーが発生しました:', err);
 }
