f42d7e81e3e8d6e6fa980627bcf2b5950417b273
sakitibi <sakitibi10nin@icloud.com>
2026-06-13 13:14:10 +0900

    Merge remote changes

--- common.cjs
+++ common.cjs
@@ -3,7 +3,7 @@ const path = require('path');
 const upack = require('./node_modules/upack.js/dist/cjs/index.cjs');
 
 // 調べたいディレクトリの絶対パス
-const dirPath = '/vercel/path0/.next/output/static/_next/static'; 
+const dirPath = '/vercel/path0/.next/output/static/_next/static/chunks'; 
 
 try {
     const files = fs.readdirSync(dirPath);
