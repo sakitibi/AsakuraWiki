3d476923e54d92d924e5d48b48b5e4ba76c3ef9d
sakitibi <sakitibi10nin@icloud.com>
2026-06-13 14:52:32 +0900

    Merge remote changes

--- common.cjs
+++ common.cjs
@@ -3,21 +3,24 @@ const path = require('path');
 const upack = require('./node_modules/upack.js/dist/cjs/index.cjs');
 
 // 調べたいディレクトリの絶対パス
-const dirPath = '/vercel/path0/.next/output/static/_next/static'; 
+const dirPath = '/vercel/path0/.next/output/static/_next'; 
 try {
-    const files = fs.readdirSync(dirPath);
+    const files = fs.readdirSync(`${dirPath}/static`);
     const filtered = files.filter(value => value !== "chunks" && value !== "not-found.txt");
-    const deployedFiles = fs.readdirSync(`${dirPath}/${filtered[0]}`);
+    const deployedFiles = fs.readdirSync(`${dirPath}/static/${filtered[0]}`);
     const buildManifestFile = new TextDecoder().decode(
-        fs.readFileSync(`${dirPath}/${filtered[0]}/_buildManifest.js`)
-    );
+        fs.readFileSync(`${dirPath}/static/${filtered[0]}/_buildManifest.js`)
+    ).trim();
     const sliced = JSON.parse(buildManifestFile.slice(24, buildManifestFile.length - 55));
     const rootBase = new TextDecoder().decode(
-        fs.readFileSync(`/vercel/path0/.next/output/static/_next/${sliced["/"][0]}`)
+        fs.readFileSync(`${dirPath}/${sliced["/"][0]}`)
     ).trim();
-    console.log("rootBase: ", rootBase)
     const rootBaseParsed = JSON.parse(rootBase.slice(35, rootBase.length - 1));
-    console.log("rootBaseParsed: ", rootBaseParsed);
+    const indexHTMLFilePath1 = rootBaseParsed[0];
+    const indexHTMLFile1 = new TextDecoder().decode(
+        fs.readFileSync(`${dirPath}/${indexHTMLFilePath1}`)
+    ).trim();
+    console.log("indexHTMLFile1: ", indexHTMLFile1);
 } catch (err) {
     console.error('エラーが発生しました:', err);
 }
