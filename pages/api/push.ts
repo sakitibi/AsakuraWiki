2a339c65f473c3481e2d039dcdb332db398ee726
sakitibi <sakitibi10nin@icloud.com>
2026-06-18 22:49:41 +0900

    Merge remote changes

--- pages/api/push.ts
+++ pages/api/push.ts
@@ -15,39 +15,44 @@ export default async function handler(
     if (req.method === "OPTIONS") {
         return res.status(200).end();
     } else if (req.method === "PUT") {
-        if (!repo || !path) {
-            return res.status(400).send("Error 400 Bad Request");
+        try{
+            if (!repo || !path) {
+                return res.status(400).send("Error 400 Bad Request");
+            }
+            const reqbody = req.body;
+            const response0 = await fetch(`${process.env.REPO_BASE_URL}/${repo}/edit/${branch}/${path}`);
+            const data0 = await response0.text();
+            const data0_release_indexof = data0.indexOf('<meta name="release" content="');
+            const data0_nonce_indexof = data0.indexOf('<meta name="fetch-nonce" content="');
+            const req_fetch_release = data0.slice(data0_release_indexof + 30, data0_release_indexof + 70);
+            const req_fetch_nonce = data0.slice(data0_nonce_indexof + 34, data0_nonce_indexof + 73);
+            console.log("nonce: ", req_fetch_nonce);
+            const headers = PushFetchComponents1(
+                repo,
+                branch,
+                path,
+                req_fetch_nonce,
+                req_fetch_release
+            );
+            const body = PushFetchComponents2(
+                reqbody.message,
+                reqbody.path,
+                reqbody.new_path,
+                Buffer.from(reqbody.bytes, 'base64')
+            );
+            console.log("headers: ", headers);
+            console.log("body: ", body);
+            const response = await fetch(`${process.env.REPO_BASE_URL}/${repo}/edit/${branch}/${path}`, {
+                method: "POST",
+                headers,
+                body
+            });
+            const data = await response.json();
+            const res_headers = Object(response.headers);
+            return res.status(200).json({data, res_headers});
+        } catch(e) {
+            console.error("Error: ", e);
+            return res.status(500).json({error: e});
         }
-        const reqbody = req.body;
-        const response0 = await fetch(`${process.env.REPO_BASE_URL}/${repo}/edit/${branch}/${path}`);
-        const data0 = await response0.text();
-        const data0_release_indexof = data0.indexOf('<meta name="release" content="');
-        const data0_nonce_indexof = data0.indexOf('<meta name="fetch-nonce" content="');
-        const req_fetch_release = data0.slice(data0_release_indexof + 30, data0_release_indexof + 70);
-        const req_fetch_nonce = data0.slice(data0_nonce_indexof + 34, data0_nonce_indexof + 73);
-        console.log("nonce: ", req_fetch_nonce);
-        const headers = PushFetchComponents1(
-            repo,
-            branch,
-            path,
-            req_fetch_nonce,
-            req_fetch_release
-        );
-        const body = PushFetchComponents2(
-            reqbody.message,
-            reqbody.path,
-            reqbody.new_path,
-            Buffer.from(reqbody.bytes, 'base64')
-        );
-        console.log("headers: ", headers);
-        console.log("body: ", body);
-        const response = await fetch(`${process.env.REPO_BASE_URL}/${repo}/edit/${branch}/${path}`, {
-            method: "POST",
-            headers,
-            body
-        });
-        const data = await response.json();
-        const res_headers = Object(response.headers);
-        return res.status(200).json({data, res_headers});
     }
 }
