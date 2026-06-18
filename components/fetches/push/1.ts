ba347d1a71c132dc71b334e5dd9b053e9b745e19
sakitibi <sakitibi10nin@icloud.com>
2026-06-18 22:44:08 +0900

    Merge remote changeds

--- components/fetches/push/1.ts
+++ components/fetches/push/1.ts
@@ -2,19 +2,19 @@ export default function PushFetchComponents1(
     repo: string,
     branch: string,
     path: string,
-    req_fetch_nonce: string
+    req_fetch_nonce: string,
+    req_fetch_version: string
 ) {
     const headers = new Headers();
     const fullPath = `${process.env.REPO_BASE_URL}/${repo}/tree-save/${branch}/${path}`;
     headers.set("sec-ch-ua", '"Microsoft Edge";v="149", "Chromium";v="149", "Not)A;Brand";v="24"');
-    headers.set("content-type", "multipart/form-data; boundary=----WebKitFormBoundaryAPJBpVtPj5fsUiFf");
     headers.set("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36 Edg/149.0.0.0");
     headers.set("origin", process.env.REPO_BASE_URL!);
     headers.set("sec-fetch-site", "same-origin");
     headers.set("x-fetch-nonce", req_fetch_nonce);
     headers.set("sec-fetch-mode", "cors");
     headers.set("github-verified-fetch", "true");
-    headers.set("x-github-client-version", "0a9d22498b565412024c2cd95a0875a2f53510f1");
+    headers.set("x-github-client-version", req_fetch_version);
     headers.set("sec-ch-ua-platform", '"macOS"');
     headers.set("accept", "application/json");
     headers.set("x-requested-with", "XMLHttpRequest");
--- pages/api/push.ts
+++ pages/api/push.ts
@@ -21,10 +21,18 @@ export default async function handler(
         const reqbody = req.body;
         const response0 = await fetch(`${process.env.REPO_BASE_URL}/${repo}/edit/${branch}/${path}`);
         const data0 = await response0.text();
-        const data0_indexof = data0.indexOf('<meta name="fetch-nonce" content="');
-        const req_fetch_nonce = data0.slice(data0_indexof + 34, data0_indexof + 73);
+        const data0_release_indexof = data0.indexOf('<meta name="release" content="');
+        const data0_nonce_indexof = data0.indexOf('<meta name="fetch-nonce" content="');
+        const req_fetch_release = data0.slice(data0_release_indexof + 30, data0_release_indexof + 70);
+        const req_fetch_nonce = data0.slice(data0_nonce_indexof + 34, data0_nonce_indexof + 73);
         console.log("nonce: ", req_fetch_nonce);
-        const headers = PushFetchComponents1(repo, branch, path, req_fetch_nonce);
+        const headers = PushFetchComponents1(
+            repo,
+            branch,
+            path,
+            req_fetch_nonce,
+            req_fetch_release
+        );
         const body = PushFetchComponents2(
             reqbody.message,
             reqbody.path,
