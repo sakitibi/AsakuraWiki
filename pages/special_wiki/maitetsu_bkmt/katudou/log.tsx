ed8fa18845e848d72ef78c7898f1c6468a79ae95
sakitibi <sakitibi10nin@icloud.com>
2026-06-20 12:17:41 +0900

    Merge remote changes

--- pages/special_wiki/maitetsu_bkmt/katudou/log.tsx
+++ pages/special_wiki/maitetsu_bkmt/katudou/log.tsx
@@ -16,6 +16,19 @@ export default function Log(){
             }
         });
     }, []);
+    useEffect(() => {
+        (async function(){
+            const res = await fetch("https://asakura-wiki.vercel.app/api/wiki_v3/secure-decoder", {
+                method: "GET",
+                headers: {
+                    "X-Repo": "AsakuraWikiMetadatas",
+                    "X-Path": "special_wiki/maitetsu_bkmt/katudou/log"
+                }
+            });
+            const data = await res.text();
+            document.getElementById("contents")!.innerHTML = data;
+        })();
+    }, []);
     const asakura_member_list_found:string | undefined = asakuraMenberUserId.find(value => value === user?.id);
     const Disabled = () => {
         location.reload();
@@ -28,19 +41,7 @@ export default function Log(){
             <div>
                 <div style={{ padding: '2rem', maxWidth: 800 }}>
                     <div>
-                        <main id='contents'>
-                            <h1>マイ鉄ネット撲滅委員会活動ログ</h1>
-                            <ul>
-                                <li>関係無いコメント即削除 -- さきちび 2025-07-23 (水) 23:01:37</li>
-                                <li>まだ始まってないのに想定外すぎるので<br/>成功ライン25に引き上げで -- みぞれ 2025-07-24 (木) 15:19:19</li>
-                                <li>OK、 -- さきちび 2025-07-24 (木) 15:32:43</li>
-                                <li>先ほどコメントしたはずの文章が消えているのですがなぜでしょうか？ -- aaa_ 2025-07-24 (木) 17:42:27</li>
-                                <li>反逆系コメントは削除、またはコメントアウトをさせていただきます<br/>目的に反するコメントも対象<br/>ちなみにこのコメントずっと残ります(非公開で) -- さきちび 2025-07-24 (木) 17:42:46</li>
-                                <li>ちなみに25超えたんで活動大成功で -- みぞれ 2025-07-24 (木) 17:46:32</li>
-                                <li>やばい、大成功どころがサーバー落ちそう、<br/>まさか45以上も来ると思わんて～ -- さきちび 2025-07-24 (木) 18:07:29</li>
-                                <li>これから50,75,100..の順で大成功の大が増えます -- みぞれ 2025-07-24 (木) 18:11:28</li>
-                            </ul>
-                        </main>
+                        <main id='contents'></main>
                     </div>
                     <br />
                     <div>
