41558f0dd49898e95e0dc9522ee98d47460aa64c
sakitibi <sakitibi10nin@icloud.com>
2026-06-21 15:32:36 +0900

    Merge remote changes

--- pages/api/accounts/notice.ts
+++ pages/api/accounts/notice.ts
@@ -12,6 +12,7 @@ export default async function handler(req:NextApiRequest, res: NextApiResponse) 
     res.setHeader('Access-Control-Allow-Methods', 'POST,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     if (req.method === 'POST') {
+        return res.status(200).json({success: true});
     } else if (req.method === "OPTIONS") {
         return res.status(200).end();
     } else {
