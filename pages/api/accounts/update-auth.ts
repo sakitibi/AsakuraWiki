14ed7e94da872f75e232580585cc2f864803088d
sakitibi <sakitibi10nin@icloud.com>
2026-07-18 14:42:44 +0900

    Merge remote changes

--- pages/api/accounts/update-auth.ts
+++ pages/api/accounts/update-auth.ts
@@ -1,11 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseAdmin = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY! // ← 必須
-)
-
 interface AccountProps {
     userId?: string;
     email?: string;
@@ -16,10 +11,14 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    console.log("SERVICE_ROLE: ", process.env.SUPABASE_SERVICE_ROLE_KEY);
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method not allowed' })
     }
+
+    const supabaseAdmin = createClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.SUPABASE_SERVICE_ROLE_KEY! // ← 必須
+    )
 
     const { userId, email, password } = req.body as AccountProps;
 
--- pages/dashboard/accounts/modify.tsx
+++ pages/dashboard/accounts/modify.tsx
@@ -66,16 +66,13 @@ export default function ModifyPage() {
                 month: parseInt(birthday.split("-")[1], 10),
                 date: parseInt(birthday.split("-")[2], 10)
             }
-    
-            if (getAge(checkerBirthday) < 18) {
-                setGender(gender === "woman" ? "girl" : "boy");
-            }
-
+            
             setTimeout(async () => {
                 // メタデータ暗号化
                 const updatedInputs: string[] | undefined = await secureEncrypt(
                     email, password, birthday, username, countries,
-                    gender === "woman" ? "girl" : "boy", shimei
+                    getAge(checkerBirthday) < 18 ? gender === "woman" ? "girl" : "boy" : gender,
+                    shimei
                 );
 
                 // 暗号化メタデータ送信
