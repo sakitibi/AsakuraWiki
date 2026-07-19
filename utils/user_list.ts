e1d6e9f84cb207390cace6f731b08f330f8fa002
sakitibi <sakitibi10nin@icloud.com>
2026-07-19 09:40:29 +0900

    Merge remote changes

--- utils/user_list.ts
+++ utils/user_list.ts
@@ -95,7 +95,7 @@ export const adminerUserId:string[] = [ // 管理者のuser_idリスト
     "b23a6b6a-9ec2-4ea8-b492-371d0dd7c958",
     "ba159f68-814f-4564-935f-cea46624fd53",
     "96b00cd8-d43b-4a5b-b473-84433c0b3c98",
-    "0d7d5277-3447-4bf1-b5f6-748e169117d3",
+    "232e8a18-b82f-46e3-983c-4f2abfb37e7e",
     "a82a5ff2-6918-4205-8b00-a443df1e65da",
     "b31cd12c-5312-4a4d-aed6-0f0f98babb7e"
 ];
@@ -105,7 +105,7 @@ export const asakuraMenberUserId:string[] = [
     "b23a6b6a-9ec2-4ea8-b492-371d0dd7c958",
     "ba159f68-814f-4564-935f-cea46624fd53",
     "96b00cd8-d43b-4a5b-b473-84433c0b3c98",
-    "0d7d5277-3447-4bf1-b5f6-748e169117d3",
+    "232e8a18-b82f-46e3-983c-4f2abfb37e7e",
     "a82a5ff2-6918-4205-8b00-a443df1e65da",
     "b31cd12c-5312-4a4d-aed6-0f0f98babb7e"
 ]
