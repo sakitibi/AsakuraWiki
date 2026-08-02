922477892041fc5eb8019b4b9d6e221ab4f080c0
sakitibi <sakitibi10nin@icloud.com>
2026-08-02 23:31:24 +0900

    Merge remote changes

---
+++ pages/news/2026/08/02/1.tsx
@@ -0,0 +1,55 @@
+import Head from 'next/head';
+import styles from '@/css/index.module.css';
+import LeftMenuJp from '@/utils/pageParts/top/jp/LeftMenu';
+import MenuJp from '@/utils/pageParts/top/jp/Menu';
+import RightMenuJp from '@/utils/pageParts/top/jp/RightMenu';
+import { useState, useEffect } from 'react';
+import HeaderJp from '@/utils/pageParts/top/jp/Header';
+import FooterJp from '@/utils/pageParts/top/jp/Footer';
+import { GetServerSideProps } from 'next';
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+    res.setHeader(
+        'Cache-Control',
+        'public, s-maxage=604800, stale-while-revalidate=59'
+    );
+
+    return {
+        props: {},
+    };
+}
+
+export default function NewsPage() {
+    const [menuStatus, setMenuStatus] = useState(false);
+    useEffect(() => {
+        if(typeof document !== "undefined"){
+            document.body.style.overflow = menuStatus ? "hidden" : "";
+            return () => {
+                document.body.style.overflow = "";
+            };
+        }
+    }, [menuStatus]);
+    const handleClick = () => {
+        setMenuStatus(prev => !prev);
+    };
+    return (
+        <>
+            <Head>
+                <title>2026/08/02 明日、第十四回退社式を実行します</title>
+            </Head>
+            <MenuJp handleClick={handleClick} menuStatus={menuStatus}/>
+            <div className={styles.contentsWrapper}>
+                <HeaderJp handleClick={handleClick}/>
+                <div className={styles.contents}>
+                    <LeftMenuJp URL="/news/2026/08/02/1"/>
+                    <main style={{ padding: '2rem', flex: 1 }}>
+                        <h1>2026/08/02 明日、第十四回退社式を実行します</h1>
+                        <p><a href="https://sakitibi.github.io/14nin.com/events/graduation/2026-2/1.html"></a>詳しくはこちら</p>
+                    </main>
+                    <RightMenuJp/>
+                </div>
+                <FooterJp/>
+            </div>
+        </>
+    )
+}
