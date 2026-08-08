4f205e68e851a823acf6b5880a0edf2a83886140
sakitibi <sakitibi10nin@icloud.com>
2026-08-08 22:52:10 +0900

    Merge remote changes

--- pages/ru/index.tsx
+++ pages/ru/index.tsx
@@ -44,7 +44,7 @@ export default function Home() {
 
         const ua = navigator.userAgent;
         const bot =
-            /(Googlebot|Google-InspectionTool|AdsBot-Google|bingbot|Slurp|DuckDuckBot|YandexBot|Baiduspider|facebook.com)/i.test(ua);
+            /(Googlebot|Google-InspectionTool|AdsBot-Google|bingbot|Slurp|DuckDuckBot|YandexBot|Baiduspider|facebook\.com)/i.test(ua);
 
         setIsBot(bot);
 
