a7b49c771cc53b895b8630e29fc1de411a200ead
sakitibi <sakitibi10nin@icloud.com>
2026-08-25 15:20:24 +0900

    Merge remote changes

--- pages/special_wiki/14ninstudio/station_name_bosyu.tsx
+++ pages/special_wiki/14ninstudio/station_name_bosyu.tsx
@@ -45,7 +45,6 @@ export default function StationNameBosyu() {
                                     <p>基本的に上から順に採用されます。</p>
                                 </summary>
                                 <ul>
-                                    <li>葵</li>
                                     <li>竹内</li>
                                     <li>百花</li>
                                     <li>愛実</li>
