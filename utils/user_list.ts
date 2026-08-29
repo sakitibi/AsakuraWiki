c0c17b220359587cdc5640b900f923721cb89ca7
sakitibi <sakitibi10nin@icloud.com>
2026-08-29 19:05:02 +0900

    Merge remote changes

--- utils/user_list.ts
+++ utils/user_list.ts
@@ -92,16 +92,16 @@ export const blockedIP:RegExp[] = [ // ブロックするipリスト
 
 export const adminerUserId:string[] = [ // 管理者のuser_idリスト
     "9d2347a2-6322-4d55-93f1-71d8440e5f32",
-    "254d7617-216e-44ad-ba11-367506ef6034",
+    "cbbccb1c-ab8f-4b67-b903-fb75cf26c60a",
     "ba159f68-814f-4564-935f-cea46624fd53",
     "96b00cd8-d43b-4a5b-b473-84433c0b3c98",
-    "e0791a44-563b-46e5-920c-bd9e566a729b",
+    "f06de8f9-7ae1-4e69-8c62-7dbf40708137",
 ];
 
 export const asakuraMenberUserId:string[] = [
     "9d2347a2-6322-4d55-93f1-71d8440e5f32",
-    "254d7617-216e-44ad-ba11-367506ef6034",
+    "cbbccb1c-ab8f-4b67-b903-fb75cf26c60a",
     "ba159f68-814f-4564-935f-cea46624fd53",
     "96b00cd8-d43b-4a5b-b473-84433c0b3c98",
-    "e0791a44-563b-46e5-920c-bd9e566a729b",
+    "f06de8f9-7ae1-4e69-8c62-7dbf40708137",
 ]
