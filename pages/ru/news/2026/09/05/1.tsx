26af38feac64140987c1f3f406f274156aa9aca9
sakitibi <sakitibi10nin@icloud.com>
2026-09-05 18:44:09 +0900

    Merge remote changes

---
+++ pages/ru/news/2026/09/05/1.tsx
@@ -0,0 +1,55 @@
+import Head from 'next/head';
+import styles from '@/css/index.module.css';
+import LeftMenuRu from '@/utils/pageParts/top/ru/LeftMenu';
+import MenuRu from '@/utils/pageParts/top/ru/Menu';
+import RightMenuRu from '@/utils/pageParts/top/ru/RightMenu';
+import { useState, useEffect } from 'react';
+import HeaderRu from '@/utils/pageParts/top/ru/Header';
+import FooterRu from '@/utils/pageParts/top/ru/Footer';
+import { GetServerSideProps } from 'next';
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+    res.setHeader(
+        'Cache-Control',
+        'public, s-maxage=604800, stale-while-revalidate=59'
+    );
+
+    return {
+        props: {},
+    };
+}
+
+export default function NewsPage() {
+    const [menuStatus, setMenuStatus] = useState(false);
+    useEffect(() => {
+        if(typeof document !== "undefined"){
+            document.body.style.overflow = menuStatus ? "hidden" : "";
+            return () => {
+                document.body.style.overflow = "";
+            };
+        }
+    }, [menuStatus]);
+    const handleClick = () => {
+        setMenuStatus(prev => !prev);
+    };
+    return (
+        <>
+            <Head>
+                <title>2026/09/05 В NMNGYURI объявлено чрезвычайное положение</title>
+            </Head>
+            <MenuRu handleClick={handleClick} menuStatus={menuStatus}/>
+            <div className={styles.contentsWrapper}>
+                <HeaderRu handleClick={handleClick}/>
+                <div className={styles.contents}>
+                    <LeftMenuRu URL="/news/2026/09/05/1"/>
+                    <main style={{ padding: '2rem', flex: 1 }}>
+                        <h1>2026/09/05 В NMNGYURI объявлено чрезвычайное положение</h1>
+                        <p><a href="https://sakitibi.github.io/14nin.com/nmng_kinkyujitai">Для получения дополнительной информации нажмите здесь</a></p>
+                    </main>
+                    <RightMenuRu/>
+                </div>
+                <FooterRu/>
+            </div>
+        </>
+    )
+}
