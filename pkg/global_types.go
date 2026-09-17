552838916fe32a9b5e1f9652175cf2d1013f5d00
sakitibi <sakitibi10nin@icloud.com>
2026-09-17 14:03:48 +0900

    Merge remote changes

--- pkg/global_types.go
+++ pkg/global_types.go
@@ -9,5 +9,5 @@ var AdminerUserId = map[string]bool{
 	"cbbccb1c-ab8f-4b67-b903-fb75cf26c60a": true,
 	"ba159f68-814f-4564-935f-cea46624fd53": true,
 	"96b00cd8-d43b-4a5b-b473-84433c0b3c98": true,
-	"f06de8f9-7ae1-4e69-8c62-7dbf40708137": true,
+	"b2f3d4bd-3d50-48ee-8393-8509c754c6d3": true,
 }
--- utils/user_list.ts
+++ utils/user_list.ts
@@ -95,7 +95,7 @@ export const adminerUserId:string[] = [ // 管理者のuser_idリスト
     "cbbccb1c-ab8f-4b67-b903-fb75cf26c60a",
     "ba159f68-814f-4564-935f-cea46624fd53",
     "96b00cd8-d43b-4a5b-b473-84433c0b3c98",
-    "f06de8f9-7ae1-4e69-8c62-7dbf40708137",
+    "b2f3d4bd-3d50-48ee-8393-8509c754c6d3",
 ];
 
 export const asakuraMenberUserId:string[] = [
@@ -103,5 +103,5 @@ export const asakuraMenberUserId:string[] = [
     "cbbccb1c-ab8f-4b67-b903-fb75cf26c60a",
     "ba159f68-814f-4564-935f-cea46624fd53",
     "96b00cd8-d43b-4a5b-b473-84433c0b3c98",
-    "f06de8f9-7ae1-4e69-8c62-7dbf40708137",
+    "b2f3d4bd-3d50-48ee-8393-8509c754c6d3",
 ]
